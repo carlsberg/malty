914c053ad5048588cea36cf7930190b8ce00f5c9
Vicente Opaso <vicente@opa.so>
2021-04-13 01:14:07 +0200

    Restructure

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
-    "../ui/**/*.stories.mdx",
-    "../ui/**/*.stories.@(js|jsx|ts|tsx)"
+    "../components/**/**/*.stories.mdx",
+    "../components/**/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
