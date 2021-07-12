622ff48bc8310fb4652be9773be4b7a0c728f84d
Julian <jve@novicell.com>
2021-07-12 14:11:21 +0200

    Switch text color to default instead of primary.

--- malty/atoms/Text/Text.styled.ts
+++ malty/atoms/Text/Text.styled.ts
@@ -3,7 +3,7 @@ import { Align, Color, Size, Weight } from './Text.types';
 
 export const StyledParagraph = styled.p<{ size?: Size; weight?: string; align?: string; color?: Color }>`
   font-family: ${({ theme }) => theme.font.fontFamily.text};
-  color: ${({ color, theme }) => (color ? theme.color[color].value : theme.color.theme.themePrimary.value)};
+  color: ${({ color, theme }) => (color ? theme.color[color].value : theme.color.default.value)};
   text-align: ${({ align }) => align ?? Align.Left};
   font-size: ${({ size, theme }) => (size ? theme.font.fontSizes[size] : theme.font.fontSizes.medium)};
   font-weight: ${({ weight }) => weight || Weight.Normal};
