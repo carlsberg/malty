05fcf51b419d32393227804890637ee3b6da7d1a
Carl Sare <geloers@gmail.com>
2021-08-02 16:06:05 +0800

    Update test and snapshot

--- malty/molecules/Loading/Loading.test.tsx
+++ malty/molecules/Loading/Loading.test.tsx
@@ -3,10 +3,6 @@ import React from 'react';
 import { Loading } from './Loading';
 import { LoadingStatus, SizeTypes } from './Loading.types';
 
-jest.mock('./Loading', () => ({
-  v4: () => <Loading text="Loading..." size={SizeTypes.Medium} status={LoadingStatus.Pending} />
-}));
-
 describe('molecule loading', () => {
   it('matches snapshot', () => {
     const view = jsonRenderer(<Loading text="Loading..." size={SizeTypes.Medium} status={LoadingStatus.Pending} />);
@@ -16,10 +12,5 @@ describe('molecule loading', () => {
   it('renders element with text', () => {
     render(<Loading text="Loading..." size={SizeTypes.Medium} status={LoadingStatus.Pending} />);
     expect(screen.getByText('Loading...')).toBeInTheDocument();
-  });
-
-  it('renders element only', () => {
-    render(<Loading size={SizeTypes.Medium} status={LoadingStatus.Pending} />);
-    expect(screen.getByTestId('svg-component')).toBeInTheDocument();
   });
 });
