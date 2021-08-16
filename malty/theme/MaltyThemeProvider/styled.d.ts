5734c8da4db4299757e8584a74df0bf7674ba7ec
Vicente Opaso <vicente@opa.so>
2021-08-16 10:26:41 +0200

    Implement typography and variables JSON files

--- malty/theme/MaltyThemeProvider/styled.d.ts
+++ malty/theme/MaltyThemeProvider/styled.d.ts
@@ -4,101 +4,314 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     color: Colors;
     font: Font;
+    typography: TypographyTypes;
+    variables: VariablesType;
   }
 
-  interface Font {
-    fontSizes: FontSizes;
-    fontFamily: FontFamily;
+  interface VariablesType {
+    button: ButtonType;
+    checkbox: CheckboxType;
+    icon: IconType;
+    input: InputType;
+    pill: PillType;
+    progressBar: ProgressBarType;
+    tooltip: TooltipType;
+    codeInput: CodeInputType;
+    loading: LoadingType;
   }
 
-  interface FontSizes {
-    small: string;
-    medium: string;
-    large: string;
+  interface ButtonType {
+    icon: ButtonIconType;
+    size: ButtonSize;
   }
-  interface FontFamily {
-    headings: string;
-    text: string;
+
+  interface ButtonSize {
+    small: PropValue;
+    medium: PropValue;
+    large: PropValue;
+    xlarge: PropValue;
+  }
+
+  interface ButtonIconType {
+    size: ButtonIconSizeType;
+    margin: PropValue;
+  }
+
+  interface ButtonIconSizeType {
+    medium: PropValue;
+    small: PropValue;
+  }
+
+  interface CheckboxType {
+    size: PropValue;
+    label: CheckboxLabelType;
+    border: PropValue;
+    borderRadius: PropValue;
+    error: CheckboxErrorType;
+  }
+
+  interface CheckboxLabelType {
+    leftPadding: PropValue;
+  }
+
+  interface CheckboxErrorType {
+    padding: PropValue;
+  }
+
+  interface IconType {
+    size: IconSizeType;
+  }
+
+  interface InputType {
+    size: InputSizeType;
+    padding: PropValue;
+    largePadding: PropValue;
+    leftPadding: PropValue;
+    iconPadding: PropValue;
+    label: InputLabelType;
+    number: InputNumberType;
+    select: InputSelectType;
+  }
+
+  interface PillType {
+    size: PillSizeType;
+    padding: PropValue;
+  }
+
+  interface PillSizeType {
+    xsmall: PropValue;
+    small: PropValue;
+    medium: PropValue;
+    large: PropValue;
+  }
+
+  interface ProgressBarType {
+    bar: ProgressBarBarType;
+    text: ProgressBarTextType;
+  }
+
+  interface ProgressBarBarType {
+    height: PropValue;
+    borderRadius: PropValue;
+  }
+
+  interface ProgressBarTextType {
+    marginLeft: PropValue;
+    fontSize: PropValue;
+    lineHeight: PropValue;
+    marginTop: PropValue;
+  }
+
+  interface TooltipType {
+    arrowSize: PropValue;
+    horizontalPosTop: PropValue;
+    horizontalPosTopTranslateY: PropValue;
+    verticalPosLeft: PropValue;
+    verticalPosLeftTranslateX: PropValue;
+    padding: PropValue;
+    minWidth: PropValue;
+    boxShadow: PropValue;
+    topPosition: TooltipPositionTopType;
+    bottomPosition: TooltipPositionBottomType;
+    rightPosition: TooltipPositionRightType;
+    leftPosition: TooltipPositionLeftType;
+  }
+
+  interface TooltipPositionTopType {
+    bottom: PropValue;
+    borderWidth: PropValue;
+  }
+
+  interface TooltipPositionBottomType {
+    top: PropValue;
+    borderWidth: PropValue;
+  }
+
+  interface TooltipPositionRightType {
+    left: PropValue;
+    borderWidth: PropValue;
+  }
+
+  interface TooltipPositionLeftType {
+    right: PropValue;
+    borderWidth: PropValue;
+  }
+
+  interface CodeInputType {
+    size: CodeInputSizeType;
+    paddingBottom: PropValue;
+    paddingTop: PropValue;
+    icontTop: PropValue;
+    iconTopTransform: PropValue;
+    placeholderOpacity: PropValue;
+    iconLargePadding: PropValue;
+    iconSmallPadding: PropValue;
+  }
+
+  interface CodeInputSizeType {
+    large: PropValue;
+    medium: PropValue;
+  }
+
+  interface LoadingType {
+    size: LoadingSizeType;
+    gap: PropValue;
+    padding: PropValue;
+    color: PropValue;
+    letterSpacing: PropValue;
+    lineHeight: PropValue;
+  }
+
+  interface LoadingSizeType {
+    small: PropValue;
+    medium: PropValue;
+    large: PropValue;
+  }
+
+  interface InputNumberType {
+    width: PropValue;
+  }
+
+  interface InputSelectType {
+    width: PropValue;
+  }
+
+  interface InputLabelType {
+    bottomPadding: PropValue;
+  }
+
+  interface InputSizeType {
+    medium: PropValue;
+    large: PropValue;
+  }
+
+  interface IconSizeType {
+    small: PropValue;
+    medium: PropValue;
+    large: PropValue;
+  }
+  interface TypographyTypes {
+    global: GlobalTypography;
+    headline: HeadlineTypographySizes;
+    information: InformationTypographySizes;
+    text: TextTypographySizes;
+  }
+
+  interface GlobalTypography {
+    'font-family': PropValue;
+  }
+
+  interface HeadlineTypographySizes {
+    display: TypographySize;
+    xlarge: TypographySize;
+    hero: TypographySize;
+    huge: TypographySize;
+    large: TypographySize;
+    medium: TypographySize;
+    small: TypographySize;
+  }
+
+  interface InformationTypographySizes {
+    micro: TypographySize;
+    small: TypographySize;
+    tiny: TypographySize;
+  }
+
+  interface TextTypographySizes {
+    medium: TypographySize;
+    'medium-small': TypographySize;
+    small: TypographySize;
+  }
+
+  interface TypographySize {
+    'font-size': PropValue;
+    'line-height': PropValue;
+  }
+
+  interface TypographyAlignment {
+    left: boolean;
+    center: boolean;
+    right: boolean;
   }
 
   interface Colors {
-    theme?: Theme;
+    theme: Theme;
     button: ButtonColors;
     data: DataColors;
-    default: ColorValue;
+    default: PropValue;
     form: FormColors;
     gradient: GradientColors;
     information: InformationColors;
     overlay: OverlayColors;
     support: SupportColors;
     system: SystemColors;
-    transparent: ColorValue;
-    white: ColorValue;
-    black: ColorValue;
+    transparent: PropValue;
+    white: PropValue;
+    black: PropValue;
   }
 
-  interface ColorValue {
+  interface PropValue {
     value: string;
   }
 
   interface OpacityPair {
-    default: ColorValue;
-    white: ColorValue;
+    default: PropValue;
+    white: PropValue;
   }
 
   interface Theme {
-    themePrimary: ColorValue;
-    themeSecondary: ColorValue;
-    themeTertiary: ColorValue;
+    themePrimary: PropValue;
+    themeSecondary: PropValue;
+    themeTertiary: PropValue;
   }
 
   interface ButtonColors {
-    primaryDefault: ColorValue;
-    primaryDisable: ColorValue;
-    primaryHover: ColorValue;
-    primaryNegativeDefault: ColorValue;
-    primaryNegativeHover: ColorValue;
+    primaryDefault: PropValue;
+    primaryDisable: PropValue;
+    primaryHover: PropValue;
+    primaryNegativeDefault: PropValue;
+    primaryNegativeHover: PropValue;
   }
 
   interface DataColors {
-    lightGreen1: ColorValue;
-    trueBlue2: ColorValue;
-    naplesYellow3: ColorValue;
-    vividRed4: ColorValue;
-    skyBlue5: ColorValue;
-    goldYellow6: ColorValue;
-    carlsbergGreen7: ColorValue;
-    apricot8: ColorValue;
+    lightGreen1: PropValue;
+    trueBlue2: PropValue;
+    naplesYellow3: PropValue;
+    vividRed4: PropValue;
+    skyBlue5: PropValue;
+    goldYellow6: PropValue;
+    carlsbergGreen7: PropValue;
+    apricot8: PropValue;
   }
 
   interface FormColors {
-    calendarAvailable: ColorValue;
-    calendarSelect: ColorValue;
-    calendarSpecial: ColorValue;
-    formSelect: ColorValue;
+    calendarAvailable: PropValue;
+    calendarSelect: PropValue;
+    calendarSpecial: PropValue;
+    formSelect: PropValue;
   }
 
   interface GradientColors {
-    themePrimaryHorizontal: ColorValue;
-    themePrimaryVertical: ColorValue;
-    transparent: ColorValue;
-    whiteHorizontal: ColorValue;
-    whiteVertical: ColorValue;
+    themePrimaryHorizontal: PropValue;
+    themePrimaryVertical: PropValue;
+    transparent: PropValue;
+    whiteHorizontal: PropValue;
+    whiteVertical: PropValue;
   }
 
   interface InformationColors {
-    active: ColorValue;
-    archive: ColorValue;
-    closed: ColorValue;
-    disable: ColorValue;
-    hold: ColorValue;
-    indirect: ColorValue;
-    live: ColorValue;
-    multiple: ColorValue;
-    new: ColorValue;
-    parked: ColorValue;
-    prospect: ColorValue;
-    wholesaler: ColorValue;
+    active: PropValue;
+    archive: PropValue;
+    closed: PropValue;
+    disable: PropValue;
+    hold: PropValue;
+    indirect: PropValue;
+    live: PropValue;
+    multiple: PropValue;
+    new: PropValue;
+    parked: PropValue;
+    prospect: PropValue;
+    wholesaler: PropValue;
   }
 
   interface OverlayColors {
@@ -106,27 +319,92 @@ declare module 'styled-components' {
     opacity50: OpacityPair;
     opacity75: OpacityPair;
     opacity90: OpacityPair;
-    transparent: ColorValue;
+    transparent: PropValue;
   }
 
   interface SupportColors {
-    support20: ColorValue;
-    support40: ColorValue;
-    support60: ColorValue;
-    support80: ColorValue;
+    support20: PropValue;
+    support40: PropValue;
+    support60: PropValue;
+    support80: PropValue;
   }
 
   interface SystemColors {
-    alertBackground: ColorValue;
-    alertStrong: ColorValue;
-    disableBackground: ColorValue;
-    disableStrong: ColorValue;
-    failBackground: ColorValue;
-    failStrong: ColorValue;
-    notificationBackground: ColorValue;
-    notificationStrong: ColorValue;
-    notificationTest: ColorValue;
-    successBackground: ColorValue;
-    successStrong: ColorValue;
+    alertBackground: PropValue;
+    alertStrong: PropValue;
+    disableBackground: PropValue;
+    disableStrong: PropValue;
+    failBackground: PropValue;
+    failStrong: PropValue;
+    notificationBackground: PropValue;
+    notificationStrong: PropValue;
+    notificationTest: PropValue;
+    successBackground: PropValue;
+    successStrong: PropValue;
   }
+
+  /**
+   * WIP adapting the typography export from
+   * InVision DSM to work for typography styling.
+
+  interface Typography {
+    headline: TypographyColors;
+    information: TypographyColors;
+    text: TypographyColors;
+  }
+
+  interface TypographyColors {
+    primary?: TypographyAlignment;
+    support?: TypographyAlignment;
+    white?: TypographyAlignment;
+    disable?: TypographyAlignment;
+    alert?: [string, TypographyStyles];
+    success?: [string, TypographyStyles];
+    fail?: [string, TypographyStyles];
+  }
+
+  interface TypographyAlignment {
+    center?: [string, TypographyStyles];
+    left?: [string, TypographyStyles];
+  }
+
+  interface TypographyStyles {
+    'font-family': FontFamily;
+    'font-size': FontSize;
+    'font-style': FontStyle;
+    'font-weight': FontWeight;
+    'line-height': LineHeight;
+    'text-align': TextAlign;
+  }
+
+  interface FontSizes {
+    small: FontSize;
+    medium: FontSize;
+    large: FontSize;
+  }
+
+  interface FontFamily {
+    value: string;
+  }
+
+  interface FontSize {
+    value: string;
+  }
+
+  interface FontStyle {
+    value: string;
+  }
+
+  interface FontWeight {
+    value: string;
+  }
+
+  interface LineHeight {
+    value: string;
+  }
+
+  interface TextAlign {
+    value: string;
+  }
+  */
 }
--- malty/theme/MaltyThemeProvider/theme.ts
+++ malty/theme/MaltyThemeProvider/theme.ts
@@ -1,38 +1,45 @@
 import { DefaultTheme } from 'styled-components';
 
-/* 
+/*
   The following requires() are not the proper solution
   for this, but unfortunately when imports are used instead
   (proper), there is an error that the files are not found.
   This stems from the JSON file format, but all config around
   consuming JSON files was implemented, and yet the issue
   remains.
+  TODO: Change require() to import statements
 */
 
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const nonThemeColors = require('./colors.json');
+const nonThemeTypography = require('./typography.json');
+const nonThemeVariables = require('./variables.json');
 const baltikaMarket = require('./themes/baltika.json');
 const cadiMarket = require('./themes/cadi.json');
 const carlsbergMarket = require('./themes/carlsberg.json');
 const globalMarket = require('./themes/global.json');
 const lbcMarket = require('./themes/lbc.json');
 
-const font = {
-  fontSizes: {
-    small: '12px',
-    medium: '16px',
-    large: '24px'
-  },
-  fontFamily: {
-    headings: `'Montserrat', Arial, sans-serif`,
-    text: `'Montserrat', Arial, sans-serif`
-  }
-};
 const defaultTheme: DefaultTheme = {
   ...nonThemeColors,
-  font
+  ...nonThemeVariables,
+  ...nonThemeTypography
 };
+
+/*
+  Cloning the defaultTheme to the market/brand themes was
+  resulting in all themes keeping a reference to the
+  defaultTheme, and this meant that every change to a
+  market/brand theme was bubbling up to the defaultTheme.
+  In other words, the last theme being modified was overwriting
+  not only every other market/brand theme, but also the defaultTheme.
+  To solve this issue we are doing a JSON Deep Clone, as explained
+  here: https://www.samanthaming.com/tidbits/70-3-ways-to-clone-objects/#_3-using-json
+  This is not ideal, but it doesn't really present any real
+  technical debt, or concerns. It should, nonetheless, be optimized
+  to a more efficient way of doing this.
+*/
 
 const baltikaTheme = JSON.parse(JSON.stringify(defaultTheme));
 const cadiTheme = JSON.parse(JSON.stringify(defaultTheme));
@@ -47,6 +54,8 @@ const lbcTheme = JSON.parse(JSON.stringify(defaultTheme));
   When changed to old-school square brackets, it just magically
   works. We should look into the types here, as I'm suspect the
   the issue is there. But I couldn't really find the culprit.
+  TODO: Test dot notation after the above issue was resolved,
+  this may not be an issue afterall.
 */
 
 /* eslint-disable dot-notation */
