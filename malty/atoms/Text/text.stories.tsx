861d5dc4fb823f8149bf448059ccf3fe31b5c127
Vicente Opaso <vicente@opa.so>
2021-08-22 15:08:21 +0200

    Update text.stories.tsx

--- malty/atoms/Text/text.stories.tsx
+++ malty/atoms/Text/text.stories.tsx
@@ -34,17 +34,15 @@ export default {
       control: {
         type: 'select'
       }
-    },
-    content: {
-      control: { type: 'text' }
     }
   }
 };
-const Template: Story<TextProps> = () => (
-  <Text>
+const Template: Story<TextProps> = (args) => (
+  <Text {...args}>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque lorem augue, cursus ac sem in, fringilla
     sagittis ligula. Curabitur viverra laoreet convallis. Nam mi tortor, pellentesque sollicitudin pretium in, lacinia
     ut nunc.
   </Text>
 );
 export const Main = Template.bind({});
+Main.args = {};
