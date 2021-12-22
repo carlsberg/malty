a0c4c7b8325a80d076b0e055e0e0896969c1247a
Vitor Martins <ptmatvit@carlsberggroup.com>
2021-12-22 14:35:13 +0000

    [MS-22] upd stories AlertComponent
    Co-authored-by: Vicente Opaso <vicente@opa.so>

--- malty/atoms/Alert/Alert.stories.tsx
+++ malty/atoms/Alert/Alert.stories.tsx
@@ -1,7 +1,7 @@
 import { action } from '@storybook/addon-actions';
 import { Meta, Story } from '@storybook/react';
 import React from 'react';
-import { Alert } from './Alert';
+import { Alert as AlertComponent } from './Alert';
 import { AlertBackgroundColor, AlertProps, AlertType, HeightSizeTypes } from './Alert.types';
 
 export default {
