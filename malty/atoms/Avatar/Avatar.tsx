9099d40c9ae020bb810f1db6ecde84d8aa203011
Catarina Fernandes <catarina.fernandes@carlsberg.com>
2022-01-27 22:21:30 +0000

    Avatar component:: display only first initial if user last name is not available

--- malty/atoms/Avatar/Avatar.tsx
+++ malty/atoms/Avatar/Avatar.tsx
@@ -8,17 +8,20 @@ const displayInitials = (username: string) => {
   }
   const nameArray = username.split(' ');
   const firstName = nameArray[0];
-  const lastName = nameArray[nameArray.length - 1];
   const firstInitial = firstName.charAt(0);
+
+  if (nameArray.length === 1) {
+    return firstInitial;
+  }
+
+  const lastName = nameArray[nameArray.length - 1];
   const lastInitial = lastName.charAt(0);
 
   return `${firstInitial} ${lastInitial}`;
 };
 
 export const Avatar = ({ profileImg, username, fontSize }: AvatarProps) => (
-  <div style={{ width: '100px' }}>
-    <StyledAvatar profileImg={profileImg} fontSize={fontSize}>
-      {!profileImg && username && <span> {displayInitials(username)} </span>}
-    </StyledAvatar>
-  </div>
+  <StyledAvatar profileImg={profileImg} fontSize={fontSize}>
+    {!profileImg && username && <span> {displayInitials(username)} </span>}
+  </StyledAvatar>
 );
