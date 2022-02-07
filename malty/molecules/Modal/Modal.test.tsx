2a006db215a8f35edc45b7bfbb2f997d70653c70
Diogo Marques <ptmardio@carlsberggroup.com>
2022-02-07 11:53:31 +0000

    modal tests update

--- malty/molecules/Modal/Modal.test.tsx
+++ malty/molecules/Modal/Modal.test.tsx
@@ -4,30 +4,28 @@ import { jsonRenderer } from '@carlsberggroup/malty.utils.test';
 import React from 'react';
 import { Modal } from './Modal';
 
-describe('molecule modal', () => {
+const title = 'Headline';
+const text = `Paragraph block to support main headline(optional)
+And…it can have 2 lines, more than that is just boring…`;
+const icon = IconNamesTypes.ItemCheck;
+const image = 'http://placehold.it/120x120&text=image1';
+const buttons = [
+  {
+    variant: ButtonStyle.Secondary,
+    label: 'Cancel',
+    onClick: () => alert('primay button pressed')
+  },
+  {
+    variant: ButtonStyle.Primary,
+    label: 'Confirm',
+    onClick: () => alert('primay button pressed')
+  }
+];
+
+describe('does molecule modal match snapshot?', () => {
   it('matches snapshot', () => {
     const view = jsonRenderer(
-      <Modal
-        open
-        setOpen={() => false}
-        title="Headline"
-        text={`Paragraph block to support main headline(optional)
-    And…it can have 2 lines, more than that is just boring…`}
-        icon={IconNamesTypes.ItemCheck}
-        image="http://placehold.it/120x120&text=image1"
-        buttons={[
-          {
-            variant: ButtonStyle.Secondary,
-            label: 'Cancel',
-            onClick: () => alert('primay button pressed')
-          },
-          {
-            variant: ButtonStyle.Primary,
-            label: 'Confirm',
-            onClick: () => alert('primay button pressed')
-          }
-        ]}
-      />
+      <Modal open setOpen={() => false} title={title} text={text} icon={icon} image={image} buttons={buttons} />
     );
     expect(view).toMatchSnapshot();
   });
