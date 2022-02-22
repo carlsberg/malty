b2765780ad6317371831bb27d4cd6a3136468083
Vicente Opaso <vicente.opaso@carlsberg.com>
2022-02-22 17:10:47 +0100

    Individual Icons

--- malty/icons/Loading/index.tsx
+++ malty/icons/Loading/index.tsx
@@ -1,4 +1,4 @@
-import { IconColor, IconWrapper, IconWrapperProps } from '@carlsberggroup/malty.atoms.icon-wrapper';
+import { IconWrapper, IconWrapperProps } from '@carlsberggroup/malty.atoms.icon-wrapper';
 import { globalTheme as defaultTheme } from '@carlsberggroup/malty.theme.malty-theme-provider';
 import React, { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
@@ -12,11 +12,7 @@ const Loading = (props: IconWrapperProps) => {
       <path d="M0 0h24v24H0z" fill="none" />
       <path
         d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 2a8 8 0 110 16 8 8 0 010-16z"
-        fill={
-          props.color === IconColor.Primary
-            ? theme.colors.colours.default.white.value
-            : theme.colors.colours.default['digital-black'].value
-        }
+        fill={theme.colors.colours.default[props.color].value}
       />
       <path d="M12 2c5.43 0 9.848 4.327 9.996 9.72L22 12h-2a8 8 0 00-7.75-7.996L12 4z" />
     </g>
