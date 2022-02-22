1ee08c74ccd4e9fe58cf86afc5d45b5e1ee15ab5
Vicente Opaso <vicente.opaso@carlsberg.com>
2022-02-22 17:04:22 +0100

    Headline

--- malty/atoms/Headline/index.ts
+++ malty/atoms/Headline/index.ts
@@ -1,4 +1,4 @@
 export { Headline } from './Headline';
 export { StyledHeadline } from './Headline.styled';
-export { Align, Color, Size } from './Headline.types';
+export { HeadlineAlign, HeadlineColor, HeadlineStyle } from './Headline.types';
 export type { HeadlineProps } from './Headline.types';
