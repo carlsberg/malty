e4297d1fafc18d72f8b2bee0cf396488c9c179a0
CatarinaFernandes-carlsberg <64021830+CatarinaFernandes-carlsberg@users.noreply.github.com>
2022-03-08 21:54:23 +0000

    Feature/ottilia 170 side bar component (#79)
    * NavList component initial version
    
    * ProductsBar component initial version
    
    * SideNav component initial version
    
    * generated SideNav snapshot
    
    * go back to previous pathname when closing SubNav
    
    * NavList: LinkComponent refactoring; destructing item properties
    
    * added SideNav, NavList and ProductsBar components to .bitmap
    
    * fixed ts errors
    
    * added .composition and .docs files
    
    * updated snapshots
    
    * type definitions fix
    
    * ts errors fix
    
    * added key to array items
    
    * updated NavList snapshot
    
    * extracted SideNav; NavList and ProductsBar styles to theme files (colors and variables)
    
    * NavList item configuration: allow any type of component/HTML element to be passed as a custom component
    
    * Update malty/molecules/SideNav/SideNav.stories.tsx
    
    Co-authored-by: Vicente Opaso <vicente@opa.so>
    
    * Update malty/molecules/SideNav/SideNav.stories.tsx
    
    Co-authored-by: Vicente Opaso <vicente@opa.so>
    
    * Update malty/molecules/SideNav/SideNav.stories.tsx
    
    Co-authored-by: Vicente Opaso <vicente@opa.so>
    
    * Update malty/molecules/SideNav/SideNav.stories.tsx
    
    Co-authored-by: Vicente Opaso <vicente@opa.so>
    
    * removed unnecessary PropValue import from styled.d.ts
    
    * fixed syntax error on .bitmap
    
    * bitmap syntax error fix
    
    * yarn.lock update
    
    * added space to SideNav story name
    
    * disabled text selection on nav items
    
    * added items group by category feature to NavList
    
    * updates snapshots
    
    * migrated productsBar border color to colors theme file
    
    * created Avatar component
    
    * Avatar component: updated text style
    
    * added container div to Avatar component story
    
    * Avatar component:: display only first initial if user last name is not available
    
    * updated yarn lock
    
    * exported Link component from NavList
    
    * NavList:: updated nav items font size
    
    * ProductsBar:: added system settings menu
    
    * ProductsBar:: added open argument to styled components
    
    * updated SideNav story with args description
    
    * added tests to Avatar component
    
    * added avatar component colors to colors.json
    
    * ProductsBar: close profile menu when clicking outside of the element
    
    * ProductsBar: profile menu onClickOutside, useEffect when open prop changes
    
    * updated Sidenav test file with products bar configuratio
    
    * added tests file to ProductsBar component
    
    * extracted ProductsBar style variables to theme files
    
    * lifted up NavList state to SideNav so it can be accessible on the ProductsBar component as well
    
    * made productName on ProductsBar optional and navList marginTop relative to viewport height
    
    * updated yarn lock
    
    * passed required prop resetNavSTate to ProductsBar.test
    
    * snapshots updated
    
    * extracted Avatar component to branch features/DSM-168
    
    * updated yarn.lock
    
    * updated theme tokens and atoms type definitions
    
    * Update malty/molecules/SideNav/SideNav.stories.tsx
    
    Co-authored-by: Vicente Opaso <vicente@opa.so>
    
    * Update malty/molecules/ProductsBar/ProductsBar.styled.ts
    
    Co-authored-by: Vicente Opaso <vicente@opa.so>
    
    * Update malty/molecules/ProductsBar/ProductsBar.styled.ts
    
    Co-authored-by: Vicente Opaso <vicente@opa.so>
    
    * Update malty/molecules/ProductsBar/ProductsBar.styled.ts
    
    Co-authored-by: Vicente Opaso <vicente@opa.so>
    
    * removed style hardcoded values
    
    * moved sidenav with sub navigation test case to sideNav component test file
    
    * added key option to ProductsBar and NavList config
    
    Co-authored-by: Vicente Opaso <vicente@opa.so>

--- malty/molecules/SideNav/SideNav.test.tsx
+++ malty/molecules/SideNav/SideNav.test.tsx
@@ -1,6 +1,7 @@
 import { IconName } from '@carlsberggroup/malty.atoms.icon';
-import { jsonRenderer, render, screen } from '@carlsberggroup/malty.utils.test';
+import { fireEvent, jsonRenderer, render, screen } from '@carlsberggroup/malty.utils.test';
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 import { SideNav } from './SideNav';
 
 const productName = 'Ottilia';
@@ -9,6 +10,18 @@ const simpleNavigation = [
   { icon: IconName.DataTransfer, name: 'item 1', href: '/item1' },
   { icon: IconName.DataTransfer, name: 'item 2', href: '/item2' },
   { icon: IconName.DataTransfer, name: 'item 3', href: '/item3' }
+];
+
+const navWithRouterItems = [
+  { icon: IconName.DataTransfer, name: 'item 1', href: '/item1' },
+  { icon: IconName.DataTransfer, name: 'item 2', href: '/item2' },
+  {
+    icon: IconName.DataTransfer,
+    name: 'item with subnav',
+    component: Link,
+    to: '/',
+    subItems: [{ name: 'sub item 1', href: '/' }]
+  }
 ];
 
 const systemOptions = [
@@ -48,5 +61,32 @@ describe('molecule sideNav', () => {
       />
     );
     expect(screen.getByText(productName)).not.toBeNull();
+  });
+});
+
+describe('sideNav sub navigation', () => {
+  it('opens when clicking a nav item with sub items', () => {
+    render(
+      <BrowserRouter>
+        <SideNav
+          productName={productName}
+          navItems={navWithRouterItems}
+          systemOptions={systemOptions}
+          profileMenu={profileMenu}
+        />
+      </BrowserRouter>
+    );
+
+    const itemWithSubNav = screen.getByText('item with subnav');
+
+    fireEvent(
+      itemWithSubNav,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true
+      })
+    );
+
+    expect(screen.getByText('sub item 1')).toBeInTheDocument();
   });
 });
