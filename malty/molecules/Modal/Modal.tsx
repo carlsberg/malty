dfabf0d082f0d49f1e4260ed402f947763a49235
Diogo Marques <ptmardio@carlsberggroup.com>
2022-03-31 17:02:46 +0100

    Added Modal to molecules

---
+++ malty/molecules/Modal/Modal.tsx
@@ -0,0 +1,79 @@
+import { Button, ButtonSize, ButtonStyle } from '@carlsberggroup/malty.atoms.button';
+import { Headline, HeadlineAlign, HeadlineStyle } from '@carlsberggroup/malty.atoms.headline';
+import { Icon, IconColor, IconName, IconSize } from '@carlsberggroup/malty.atoms.icon';
+import { Overlay } from '@carlsberggroup/malty.atoms.overlay';
+import { globalTheme as defaultTheme } from '@carlsberggroup/malty.theme.malty-theme-provider';
+import React, { useContext } from 'react';
+import { ThemeContext } from 'styled-components';
+import {
+  StyledButtonContainer,
+  StyledButtonsWrapper,
+  StyledCloseIconContainer,
+  StyledContainer,
+  StyledContentContainer,
+  StyledModalWrapper,
+  StyledTitleContainer
+} from './Modal.styled';
+import { ModalProps, ModalSize } from './Modal.types';
+
+export const Modal = ({
+  open,
+  onClose,
+  content,
+  title,
+  dismissible = true,
+  actions,
+  size = ModalSize.Medium,
+  whiteBackground = false
+}: ModalProps) => {
+  const closeModal = () => {
+    onClose();
+  };
+  const theme = useContext(ThemeContext) || defaultTheme;
+  return (
+    <>
+      {open ? (
+        <>
+          <Overlay isWhite={whiteBackground} />
+          <StyledContainer>
+            <StyledModalWrapper theme={theme} size={size}>
+              {dismissible && (
+                <StyledCloseIconContainer onClick={closeModal} theme={theme}>
+                  <Icon name={IconName.Close} size={IconSize.Medium} color={IconColor.Primary} />
+                </StyledCloseIconContainer>
+              )}
+
+              {title && (
+                <StyledTitleContainer theme={theme}>
+                  <Headline align={HeadlineAlign.Left} headlineStyle={HeadlineStyle.Large}>
+                    {title}
+                  </Headline>
+                </StyledTitleContainer>
+              )}
+              <StyledContentContainer theme={theme}>{content}</StyledContentContainer>
+
+              {actions && Array.isArray(actions) ? (
+                <StyledButtonsWrapper theme={theme}>
+                  {actions.map((btnInstance, index: number) => (
+                    <StyledButtonContainer theme={theme} key={btnInstance.key || `button${index}`}>
+                      <Button
+                        fullWidth
+                        size={ButtonSize.Large}
+                        style={ButtonStyle[btnInstance.variant]}
+                        onClick={btnInstance.onClick}
+                      >
+                        {btnInstance.label}
+                      </Button>
+                    </StyledButtonContainer>
+                  ))}
+                </StyledButtonsWrapper>
+              ) : (
+                actions
+              )}
+            </StyledModalWrapper>
+          </StyledContainer>
+        </>
+      ) : null}
+    </>
+  );
+};
---
+++ malty/molecules/Modal/index.ts
@@ -0,0 +1,2 @@
+export { Modal } from './Modal';
+export { ModalProps, ModalSize } from './Modal.types';
