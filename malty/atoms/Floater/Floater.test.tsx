ca15f8e1f01f40f02652cdb86ba96e89f0c7a0fe
Martim Matos <martim.matos@carlsberggroup.com>
2022-04-18 17:00:29 +0100

    added floater component

---
+++ malty/atoms/Floater/Floater.test.tsx
@@ -0,0 +1,41 @@
+import { fireEvent, jsonRenderer, render, screen } from '@carlsberggroup/malty.utils.test';
+import React from 'react';
+import { Floater } from './Floater';
+
+const defaultText = 'Submit';
+const newText = 'Go';
+
+describe('button', () => {
+  it('matches snapshot', () => {
+    const onClick = jest.fn();
+    const view = jsonRenderer(<Floater text={defaultText} onClick={onClick} />);
+    expect(view).toMatchSnapshot();
+  });
+
+  it('renders with correct text', () => {
+    const { rerender } = render(<Floater text={defaultText} />);
+    expect(screen.getByText(defaultText)).not.toBeNull();
+
+    rerender(<Floater text={newText} />);
+    expect(screen.getByText(newText)).not.toBeNull();
+  });
+
+  it('renders with correct text via child', () => {
+    render(<Floater>{defaultText}</Floater>);
+    expect(screen.getByText(defaultText)).not.toBeNull();
+  });
+
+  it('calls function on click', () => {
+    const onClick = jest.fn();
+    render(<Floater text={defaultText} onClick={onClick} />);
+    fireEvent.click(screen.getByText(defaultText));
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('does not call function on click when disabled', () => {
+    const onClick = jest.fn();
+    render(<Floater text={defaultText} onClick={onClick} />);
+    fireEvent.click(screen.getByText(defaultText));
+    expect(onClick).toHaveBeenCalledTimes(0);
+  });
+});
--- malty/atoms/Floater/Floater.tsx
+++ malty/atoms/Floater/Floater.tsx
@@ -1,69 +1,23 @@
-import { Icon, IconColor, IconName, IconSize } from '@carlsberggroup/malty.atoms.icon';
-import { Loading, LoadingStatus } from '@carlsberggroup/malty.molecules.loading';
+import { Icon, IconColor, IconSize } from '@carlsberggroup/malty.atoms.icon';
 import { globalTheme as defaultTheme, TypographyProvider } from '@carlsberggroup/malty.theme.malty-theme-provider';
 import React, { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from 'styled-components';
-import { ButtonProps, ButtonSize } from '.';
-import {
-  StyledAnchor,
-  StyledFloaterButton,
-  StyledLinkButton,
-  StyledPrimaryButton,
-  StyledSecondaryButton,
-  StyledTransparentButton
-} from './Button.styled';
-import { ButtonIconPosition, ButtonStyle, ButtonType } from './Button.types';
+import { FloaterIconPosition, FloaterProps } from '.';
+import { StyledFloaterButton } from './Floater.styled';
 
 export const Floater = ({
   text,
-  style,
-  type = ButtonType.Submit,
-  isWhite = false,
-  fullWidth = false,
-  selected = false,
-  disabled,
   onClick,
-  onKeyUp,
   icon,
-  url,
-  size = ButtonSize.Medium,
-  iconPos = ButtonIconPosition.Right,
-  loading,
+  iconPos = FloaterIconPosition.Right,
   scroll,
-  error,
-  errorIcon,
-  errorText,
-  success,
-  successIcon,
-  successText,
+  negative,
   tabIndex = -1,
+  dataTestId,
   children
-}: ButtonProps) => {
-  let Component = StyledPrimaryButton;
-  let iconColor = isWhite ? IconColor.Primary : IconColor.White;
-  switch (style) {
-    case ButtonStyle.Secondary:
-      Component = StyledSecondaryButton;
-      iconColor = isWhite ? IconColor.White : IconColor.Primary;
-      break;
-    case ButtonStyle.Transparent:
-      Component = StyledTransparentButton;
-      break;
-    case ButtonStyle.Floater:
-      Component = StyledFloaterButton;
-      break;
-    case ButtonStyle.Link:
-      Component = StyledLinkButton;
-      iconColor = isWhite ? IconColor.White : IconColor.Primary;
-      break;
-    default:
-      break;
-  }
+}: FloaterProps) => {
+  const iconColor = negative ? IconColor.Primary : IconColor.White;
   const theme = useContext(ThemeContext) || defaultTheme;
-  const [numSize, setNumSize] = useState(theme.sizes.xl.value);
-  const [hPadding, _setHorizontalPadding] = useState(theme.sizes.s.value);
-  const [fontSize, setFontSize] = useState(theme.typography.desktop.text.medium_default['font-size'].value);
-  const [iconSize, setIconSize] = useState(theme.sizes.m.value);
   const [showButton, setShowButton] = useState(true);
 
   const handleScroll = () => {
@@ -75,29 +29,40 @@ export const Floater = ({
     }
   };
 
-  const renderComponent = () => (
-    
-  );
-
-
-
   useEffect(() => {
-    if (style === ButtonStyle.Floater && !!scroll) {
+    if (scroll) {
       setShowButton(false);
       document.addEventListener('scroll', handleScroll);
     }
     return () => {
       document.removeEventListener('scroll', handleScroll);
     };
-  }, [style, scroll]);
+  }, [scroll]);
 
-  return url ? (
+  return (
     <TypographyProvider>
-      <StyledAnchor target="_blank" href={url} rel="noreferrer" className={selected ? 'active' : ''}>
-        {renderComponent()}
-      </StyledAnchor>
+      <StyledFloaterButton
+        data-testid={dataTestId}
+        type="button"
+        hasText={!!text}
+        hasIcon={!!icon}
+        onClick={onClick ?? (() => window.scrollTo({ top: 0, behavior: 'smooth' }))}
+        isNegative={negative}
+        iconPos={iconPos}
+        theme={theme}
+        showButton={showButton}
+        tabIndex={tabIndex}
+      >
+        <div className="text-container">
+          {icon && iconPos === FloaterIconPosition.Left && (
+            <Icon name={icon} color={iconColor} size={IconSize.Medium} />
+          )}
+          {text || children}
+          {icon && iconPos === FloaterIconPosition.Right && (
+            <Icon name={icon} color={iconColor} size={IconSize.Medium} />
+          )}
+        </div>
+      </StyledFloaterButton>
     </TypographyProvider>
-  ) : (
-    renderComponent()
   );
 };
