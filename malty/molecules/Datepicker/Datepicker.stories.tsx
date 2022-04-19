01f2bac17279e9a8d14b25af29c848ab7bb1ad0f
CatarinaFernandes-carlsberg <64021830+CatarinaFernandes-carlsberg@users.noreply.github.com>
2022-04-19 17:48:47 +0100

    Feature/dsm 119 datepicker (#115)
    * datepicker input styling
    
    * removed react-datepicker css file import
    
    * added datepicker properties to story
    
    * added datepicker component to .bitmap
    
    * fixed datepicker test error
    
    * removed minDate from story
    
    * moved Datepicker to /molecules
    
    * added Datepicker export to index.ts
    
    * allow custom props on datepicker component
    
    * enabled type on input to choose date; styled library default input; styled range selection
    
    * added datepickerProps type to export
    
    * Update malty/molecules/Stepper/__snapshots__/Stepper.test.tsx.snap
    
    Co-authored-by: Vicente Opaso <vicente@opa.so>
    
    * reset snapshots from pull request
    
    Co-authored-by: Vicente Opaso <vicente@opa.so>

---
+++ malty/molecules/Datepicker/Datepicker.stories.tsx
@@ -0,0 +1,129 @@
+import { Meta, Story } from '@storybook/react';
+import React, { useState } from 'react';
+import { Datepicker as DatepickerComponent } from './Datepicker';
+import { DatepickerProps } from './Datepicker.types';
+
+export default {
+  title: 'Forms/Datepicker',
+  component: DatepickerComponent,
+  parameters: {
+    importObject: 'Datepicker',
+    importPath: '@carlsberggroup/malty.molecules.datepicker'
+  },
+  argTypes: {
+    label: {
+      description: 'The datepicker input label',
+      control: {
+        type: 'string'
+      }
+    },
+    startDate: {
+      description: 'Initial selected date'
+    },
+    onChange: {
+      description: 'Action to perform when clicking a calendar day'
+    },
+    minDate: {
+      description: 'disable days before defined min. date',
+      control: {
+        type: 'date'
+      }
+    },
+    maxDate: {
+      description: 'disable days after defined max. date',
+      control: {
+        type: 'date'
+      }
+    },
+    dateFormat: {
+      description: 'custom date format, default is MM/dd/yyyy',
+      control: {
+        type: 'string'
+      }
+    },
+    excludeDates: {
+      description: 'disable array of days',
+      control: {
+        type: 'array'
+      }
+    },
+    disabled: {
+      description: 'disable datepicker',
+      control: {
+        type: 'boolean'
+      }
+    },
+    selectsRange: {
+      description: 'enable date range selection',
+      control: {
+        type: 'boolean'
+      }
+    },
+    inline: {
+      description: 'display calendar without input',
+      control: {
+        type: 'boolean'
+      }
+    },
+    placeholderText: {
+      description: 'input placeholder',
+      control: {
+        type: 'string'
+      }
+    },
+    locale: {
+      description: 'iso language code',
+      control: {
+        type: 'string'
+      }
+    }
+  }
+} as Meta;
+
+const Template: Story<DatepickerProps> = ({
+  label,
+  minDate,
+  maxDate,
+  disabled,
+  locale,
+  excludeDates,
+  placeholderText,
+  selectsRange,
+  dateFormat
+}) => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const onChange = (date: Date[] | Date) => {
+    let start;
+    let end;
+
+    if (Array.isArray(date)) {
+      [start, end] = date;
+      setStartDate(start);
+      setEndDate(end);
+    }
+  };
+
+  return (
+    <div style={{ height: '400px' }}>
+      <DatepickerComponent
+        label={label}
+        onChange={selectsRange ? onChange : setStartDate}
+        startDate={startDate}
+        endDate={endDate}
+        minDate={minDate}
+        maxDate={maxDate}
+        disabled={disabled}
+        locale={locale}
+        excludeDates={excludeDates}
+        placeholderText={placeholderText}
+        selectsRange={selectsRange}
+        dateFormat={dateFormat}
+      />
+    </div>
+  );
+};
+
+export const Datepicker = Template.bind({});
+Datepicker.args = { label: 'Select date' };
