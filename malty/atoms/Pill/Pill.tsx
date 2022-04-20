ca535683aad08c2078f217c186cc40a1227a9b31
Martim Matos <95625853+MartimMatosCB@users.noreply.github.com>
2022-04-20 19:29:46 +0100

    feat: Pill component spacing fix (#144)
    * added bg color to textarea
    
    * design remarks on pills
    
    * feat: added data-testid
    
    Co-authored-by: Vicente Opaso <vicente@opa.so>

--- malty/atoms/Pill/Pill.tsx
+++ malty/atoms/Pill/Pill.tsx
@@ -2,15 +2,23 @@ import { Icon, IconColor, IconSize } from '@carlsberggroup/malty.atoms.icon';
 import { globalTheme as defaultTheme, TypographyProvider } from '@carlsberggroup/malty.theme.malty-theme-provider';
 import React, { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from 'styled-components';
-import { StyledPill } from './Pill.styled';
+import { StyledPill, StyledText } from './Pill.styled';
 import { PillColor, PillProps, PillSize } from './Pill.types';
 
-export const Pill = ({ text, icon, iconColor, color = PillColor.Primary, size = PillSize.Medium }: PillProps) => {
+export const Pill = ({
+  text,
+  icon,
+  iconColor,
+  color = PillColor.Primary,
+  size = PillSize.Medium,
+  dataTestId
+}: PillProps) => {
   const theme = useContext(ThemeContext) || defaultTheme;
   const [numSize, setNumSize] = useState(theme.sizes.l.value);
   const [fontSize, setFontSize] = useState(theme.typography.desktop.text['medium-small_bold']['font-size'].value);
   const [iconSize, setIconSize] = useState(theme.sizes.m.value);
   const [padding, setPadding] = useState(theme.sizes.s.value);
+  const [marginText, setMarginText] = useState(theme.sizes['4xs'].value);
   const [iconTextColor, setIconTextColor] = useState(iconColor || IconColor.White);
 
   useEffect(() => {
@@ -20,6 +28,7 @@ export const Pill = ({ text, icon, iconColor, color = PillColor.Primary, size = 
         setFontSize(theme.typography.desktop.text.micro_bold['font-size'].value);
         setIconSize(theme.sizes.xs.value);
         setPadding(theme.sizes['3xs'].value);
+        setMarginText(theme.sizes['5xs'].value);
         break;
       }
       case PillSize.Small: {
@@ -27,6 +36,7 @@ export const Pill = ({ text, icon, iconColor, color = PillColor.Primary, size = 
         setFontSize(theme.typography.desktop.text.tiny_bold['font-size'].value);
         setIconSize(theme.sizes.s.value);
         setPadding(theme.sizes.xs.value);
+        setMarginText(theme.sizes['4xs'].value);
         break;
       }
 
@@ -35,16 +45,14 @@ export const Pill = ({ text, icon, iconColor, color = PillColor.Primary, size = 
         setFontSize(theme.typography.desktop.text['medium-small_bold']['font-size'].value);
         setIconSize(theme.sizes.m.value);
         setPadding(theme.sizes.s.value);
+        setMarginText(theme.sizes['4xs'].value);
         break;
       }
     }
   }, [size, theme]);
 
   useEffect(() => {
-    if (
-      !iconColor &&
-      (color === PillColor.Support40 || color === PillColor.Success || color === PillColor.alertStrong)
-    ) {
+    if (!iconColor && (color === PillColor.Archive || color === PillColor.Success || color === PillColor.alertStrong)) {
       setIconTextColor(IconColor.Primary);
     }
   }, [color, theme]);
@@ -52,6 +60,7 @@ export const Pill = ({ text, icon, iconColor, color = PillColor.Primary, size = 
   return (
     <TypographyProvider>
       <StyledPill
+        data-testid={dataTestId}
         color={color}
         size={numSize}
         fontSize={fontSize}
@@ -60,9 +69,20 @@ export const Pill = ({ text, icon, iconColor, color = PillColor.Primary, size = 
         hasText={!!text}
         theme={theme}
         textColor={iconTextColor}
+        pillSize={size}
       >
-        {icon && <Icon name={icon} size={IconSize.Small} color={iconTextColor} className="pill__icon" />}
-        {text}
+        {icon && (
+          <Icon
+            data-testid={`${dataTestId}-icon`}
+            name={icon}
+            size={IconSize.Small}
+            color={iconTextColor}
+            className="pill__icon"
+          />
+        )}
+        <StyledText data-testid={`${dataTestId}-text`} theme={theme} marginText={marginText} hasText={!!text}>
+          {text}
+        </StyledText>
       </StyledPill>
     </TypographyProvider>
   );
