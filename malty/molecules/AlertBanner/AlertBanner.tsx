318d9f80d8ee14fd15c6c64254e7ca2aa8b21e0b
Luan Carlos M.A <17798410+luancma@users.noreply.github.com>
2022-08-26 13:29:56 -0300

    Update malty/molecules/AlertBanner/AlertBanner.tsx
    Co-authored-by: Vicente Opaso <vicente@opa.so>

--- malty/molecules/AlertBanner/AlertBanner.tsx
+++ malty/molecules/AlertBanner/AlertBanner.tsx
@@ -156,7 +156,7 @@ export const AlertBanner: FC<AlertBannerProps> = ({
             isWhite={currentAlert.type !== AlertBannerType.Warning}
           />
         )}
-        <MessageContainer theme={theme} breakpoint={breakpoint} data-testid="alert-banner-message-content">
+        <MessageContainer theme={theme} breakpoint={breakpoint} data-testid={`${currentAlert.dataQaId}-AlertBanner-message-content`}>
           {currentAlert.icon && renderIcon()}
           {renderMessage()}
           {!isMobile && currentAlert.action && renderAction()}
