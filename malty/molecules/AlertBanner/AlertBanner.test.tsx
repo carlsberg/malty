771ca02b8c78b036a39ee6ab2b9d1f844f812fd5
tfmteixeira <ptteitia@carlsberggroup.com>
2022-09-26 12:05:07 +0100

    fix(MS-103): fix alert banner tests (#371)
    fix(MS-103):fix alert banner tests

--- malty/molecules/AlertBanner/AlertBanner.test.tsx
+++ malty/molecules/AlertBanner/AlertBanner.test.tsx
@@ -10,6 +10,7 @@ const toggleBannerText = jest.fn();
 
 const alertsMock = [
   {
+    eid: '1',
     type: AlertBannerType.Error,
     message: 'Hello, Im the AlertBanner!',
     dataQaId: 'alert-banner',
@@ -20,6 +21,7 @@ const alertsMock = [
     icon: true
   },
   {
+    eid: '2',
     type: AlertBannerType.Information,
     message: 'Hello, Im the AlertBanner!',
     dataQaId: 'alert-banner',
@@ -30,6 +32,7 @@ const alertsMock = [
     icon: true
   },
   {
+    eid: '3',
     type: AlertBannerType.Warning,
     message: 'Hello, Im the AlertBanner!',
     dataQaId: 'alert-banner',
