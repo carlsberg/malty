7ce4d58f48409b31c3622c0c9c2ad21bb3705d37
Luan Carlos M.A <17798410+luancma@users.noreply.github.com>
2022-09-30 11:42:16 -0300

    fix(MS-106): wrong position of AlertBannar pagination arrow (#375)
    * remove margins
    
    * remove padding bottom
    
    * create validation to single item
    
    * fix desktop validation
    
    * remove unnecessary changes
    
    Co-authored-by: luancma <luancmadev@gmail.com>

--- malty/molecules/AlertBanner/AlertBanner.styled.ts
+++ malty/molecules/AlertBanner/AlertBanner.styled.ts
@@ -65,8 +65,10 @@ export const ContentRow = styled.div`
 
 export const MessageContainer = styled.div<{
   breakpoint: string;
+  applyMarginBottom: boolean;
 }>`
-  padding: ${({ theme }) => theme.sizes['2xs'].value} 0px;
+  padding-top: ${({ theme }) => theme.sizes['2xs'].value};
+  padding-bottom: ${({ theme, applyMarginBottom }) => applyMarginBottom && theme.sizes['2xs'].value};
   display: flex;
   align-items: center;
   @media (min-width: ${({ breakpoint }) => breakpoint}) {
@@ -76,6 +78,7 @@ export const MessageContainer = styled.div<{
     transform: translate(-50%, -50%);
     width: fit-content;
     max-width: calc(100% - 300px);
+    padding: ${({ theme }) => theme.sizes['2xs'].value} 0px;
   }
   svg {
     min-height: ${({ theme }) => theme.sizes.m.value};
