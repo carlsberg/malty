da3f5eace3269c96d3876c6be1c8d09ba3a4493b
Martim Matos <95625853+MartimMatosCB@users.noreply.github.com>
2022-12-07 17:17:52 +0000

    feat(DSM-462): new icons (#455)
    * feat(DSM-462): added new pack of icons
    
    * feat(DSM-462): new icons
    
    * feat(DSM-462): added EllipsisVertical icon

---
+++ malty/icons/SurveyDefault/Icon.stories.tsx
@@ -0,0 +1,8 @@
+import { IconStories } from '@carlsberggroup/malty.atoms.icon-wrapper';
+import Icon from './index';
+
+const [meta, story] = IconStories('Survey Default', Icon);
+
+export default meta;
+
+export const surveyDefault = story;
