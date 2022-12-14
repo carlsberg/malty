8f81f8d3436af86fd023d9737245a76a142ad51f
Martim Matos <95625853+MartimMatosCB@users.noreply.github.com>
2022-12-14 11:53:35 +0000

    feat(DSM-285): table (#442)
    * feat(DSM-285): basic table and pagination test
    
    * feat(DSM-285): basic table version
    
    * feat(DSM-285): fixed unit tests
    
    * feat(DSM-285): minor changes
    
    * feat(DSM-285): removed TypographyProvider
    
    * feat(DSM-285): added sort icon
    
    * feat(DSM-285): row selection, dnd,
    
    * feat(DSM-285): empty table
    
    * fixed width column for dragging row
    
    * feat(DSM-285): storybook updates
    
    * feat(DSM-285): setting initial page to 0
    
    * feat(DSM-285): table alignment
    
    * feat(DSM-285): mobile layout and DnD pages issue
    
    Co-authored-by: Martim Matos <martim.developer@hotmail.com>

--- malty/organisms/Table/Table.tsx
+++ malty/organisms/Table/Table.tsx
@@ -1,3 +1,9 @@
+/* eslint-disable no-return-assign */
+/* eslint-disable react/jsx-props-no-spreading */
+import { Checkbox } from '@carlsberggroup/malty.atoms.checkbox';
+import { Icon, IconColor, IconName, IconSize } from '@carlsberggroup/malty.atoms.icon';
+import { Text, TextColor, TextStyle } from '@carlsberggroup/malty.atoms.text';
+import { Tooltip, TooltipPlacement } from '@carlsberggroup/malty.atoms.tooltip';
 import { Pagination, PaginationType } from '@carlsberggroup/malty.molecules.pagination';
 import { globalTheme as defaultTheme } from '@carlsberggroup/malty.theme.malty-theme-provider';
 import {
@@ -5,34 +11,56 @@ import {
   flexRender,
   getCoreRowModel,
   getPaginationRowModel,
+  getSortedRowModel,
   PaginationState,
+  SortingState,
   useReactTable
 } from '@tanstack/react-table';
-import React, { ReactNode, useContext, useEffect, useState } from 'react';
+import React, { ReactNode, useContext, useEffect, useMemo, useRef, useState } from 'react';
+import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd';
 import { ThemeContext } from 'styled-components';
+import { DraggableRow } from './DraggableRow';
 import {
+  StyledFooterWrapper,
   StyledHead,
+  StyledNoRecordsWrapper,
   StyledPaginationWrapper,
   StyledRow,
+  StyledSortIcon,
   StyledTable,
   StyledTbody,
   StyledTd,
+  StyledTextWrapper,
   StyledThead
 } from './Table.styled';
-import { TableProps, TableRowProps, TableSize } from './Table.types';
+import { TableHeaderAlignment, TableProps, TableRowProps, TableSize } from './Table.types';
 
-export const Table = ({ headers, rows, onRowClick, size, paginationSize = 12, className, dataTestId }: TableProps) => {
+export const Table = ({
+  headers,
+  rows,
+  onRowClick,
+  onRowSelect = () => null,
+  size,
+  paginationSize = 12,
+  className,
+  isDraggable = false,
+  dataTestId,
+  allowSelection = false,
+  totalPagesCount,
+  onPaginationChange = () => null
+}: TableProps) => {
   const columnHelper = createColumnHelper<TableRowProps>();
   const theme = useContext(ThemeContext) || defaultTheme;
   const [data, setData] = useState([...rows]);
   const [tableSize, setTableSize] = useState(theme.sizes.xl.value);
-  // const [rowSelection, setRowSelection] = useState({});
-
-  const [{ pageIndex, pageSize }, setPagination] = React.useState<PaginationState>({
+  const [rowSelection, setRowSelection] = useState({});
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const nodesRef = useRef<HTMLTableCellElement[]>([]);
+  const [{ pageIndex, pageSize }, setPagination] = useState<PaginationState>({
     pageIndex: 0,
     pageSize: paginationSize
   });
-  const pagination = React.useMemo(
+  const pagination = useMemo(
     () => ({
       pageIndex,
       pageSize
@@ -40,74 +68,57 @@ export const Table = ({ headers, rows, onRowClick, size, paginationSize = 12, cl
     [pageIndex, pageSize]
   );
 
-  useEffect(() => {
-    setData([...rows]);
-  }, [rows]);
-
-  // const handleCheckboxHeader = (table: tableType<any>) => {
-  //   if (table.getIsSomeRowsSelected()) {
-  //     return undefined;
-  //   }
-  //   if (table.getIsAllRowsSelected()) {
-  //     return true;
-  //   }
-  //   return false;
-  // };
-  // const handleCheckboxRow = (row: Row<any>) => {
-  //   if (row.getIsSomeSelected()) {
-  //     return undefined;
-  //   }
-  //   if (row.getIsSelected()) {
-  //     return true;
-  //   }
-  //   return false;
-  // };
-
   const columns = headers.map((header) =>
     columnHelper.accessor(header.key, {
-      header: () => header.header
+      id: header.key,
+      header: () => header.header,
+      meta: header.headerAlignment
     })
   );
-
-  // useEffect(() => {
-  //   if (allowSelection) {
-  //     columns.push({
-  //       id: 'select',
-  //       header: ({ table }) => (
-  //         <Checkbox
-  //           onValueChange={table.getToggleAllRowsSelectedHandler}
-  //           value={handleCheckboxHeader(table)?.toString() as string}
-  //           checked={handleCheckboxHeader(table)}
-  //         />
-  //       ),
-  //       cell: ({ row }) => (
-  //         <Checkbox
-  //           onValueChange={row.getToggleSelectedHandler}
-  //           value={handleCheckboxRow(row)?.toString() as string}
-  //           checked={handleCheckboxRow(row)}
-  //         />
-  //       )
-  //     });
-  //   }
-  // }, []);
 
   const table = useReactTable({
     data,
     columns,
     pageCount: -1,
+    autoResetPageIndex: false,
     state: {
-      //   rowSelection
-      pagination
+      rowSelection,
+      pagination,
+      sorting
     },
-    // onRowSelectionChange: setRowSelection,s
+    onRowSelectionChange: setRowSelection,
     getCoreRowModel: getCoreRowModel(),
     onPaginationChange: setPagination,
-    getPaginationRowModel: getPaginationRowModel()
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel()
   });
+  const [dataPage, setDataPage] = useState(table.getRowModel().flatRows);
 
   const handlePageChange = (page: number | string) => {
-    if (typeof page !== 'string') table.setPageIndex(page);
+    if (typeof page !== 'string') {
+      table.setPageIndex(page);
+      onPaginationChange(page);
+    }
   };
+  const handleDragEnd = (results: DropResult) => {
+    if (!results.destination) return;
+    const tempUser = [...data];
+    const [selectRow] = tempUser.splice(results.source.index, 1);
+    tempUser.splice(results.destination.index, 0, selectRow);
+    setData(tempUser);
+  };
+
+  useEffect(() => {
+    setData([...rows]);
+  }, [rows]);
+  useEffect(() => {
+    setDataPage(table.getRowModel().flatRows);
+  }, [table.getRowModel().flatRows]);
+
+  useEffect(() => {
+    onRowSelect(table.getSelectedRowModel().flatRows.map((row) => row.original));
+  }, [rowSelection]);
 
   useEffect(() => {
     switch (size) {
@@ -128,54 +139,204 @@ export const Table = ({ headers, rows, onRowClick, size, paginationSize = 12, cl
   }, [size, theme]);
 
   return (
-    <div>
-      <StyledTable data-testid={dataTestId} className={className} theme={theme}>
-        <StyledThead data-testid={`${dataTestId}-thead`} theme={theme}>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr data-testid={`${dataTestId}-tr-${headerGroup.id}`} key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <StyledHead data-testid={`${dataTestId}-th-${header.id}`} theme={theme} key={header.id}>
-                  {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                </StyledHead>
-              ))}
-            </tr>
-          ))}
-        </StyledThead>
-        <StyledTbody theme={theme}>
-          {table.getRowModel().rows.map((row) => (
-            <StyledRow
-              data-testid={`${dataTestId}-row-${row.id}`}
-              theme={theme}
-              isClickable={!!onRowClick}
-              size={tableSize}
-              onClick={() => onRowClick && onRowClick(row.original)}
-              key={row.id}
-            >
-              {row.getVisibleCells().map((cell) => (
-                <StyledTd data-testid={`${dataTestId}-cell-${cell.id}`} theme={theme} key={cell.id}>
-                  {cell.renderValue() as ReactNode}
-                </StyledTd>
-              ))}
-            </StyledRow>
-          ))}
-        </StyledTbody>
-      </StyledTable>
+    <DragDropContext onDragEnd={(results) => handleDragEnd(results)}>
+      <div>
+        <StyledTable data-testid={dataTestId} className={className} theme={theme}>
+          <StyledThead data-testid={`${dataTestId}-thead`} theme={theme}>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr data-testid={`${dataTestId}-tr-${headerGroup.id}`} key={headerGroup.id}>
+                {isDraggable && data.length > 0 && (
+                  <StyledHead className="draggable-header" isSortable={false} theme={theme} />
+                )}
 
-      <StyledPaginationWrapper data-testid={`${dataTestId}-pagination`} theme={theme}>
-        <Pagination
-          type={PaginationType.Input}
-          count={
-            data.length / table.getState().pagination.pageSize -
-              Math.floor(data.length / table.getState().pagination.pageSize) !==
-            0
-              ? Math.trunc(data.length / table.getState().pagination.pageSize + 1)
-              : Math.trunc(data.length / table.getState().pagination.pageSize)
-          }
-          currentPage={table.getState().pagination.pageIndex}
-          onChange={(page) => handlePageChange(page)}
-          zeroBasedIndex
-        />
-      </StyledPaginationWrapper>
-    </div>
+                {allowSelection && data.length > 0 && (
+                  <StyledHead className="checkbox-header" isSortable={false} theme={theme}>
+                    <Checkbox
+                      onValueChange={table.getToggleAllRowsSelectedHandler()}
+                      checked={table.getIsAllRowsSelected()}
+                      isIndeterminate={table.getIsSomeRowsSelected()}
+                    />
+                  </StyledHead>
+                )}
+                {headerGroup.headers.map((header, index) => (
+                  <StyledHead
+                    alignPosition={columns[index].meta as TableHeaderAlignment | undefined}
+                    ref={(elem: HTMLTableCellElement) => (nodesRef.current[index] = elem)}
+                    isSortable={header.column.getCanSort()}
+                    onClick={header.column.getToggleSortingHandler()}
+                    data-testid={`${dataTestId}-th-${header.id}`}
+                    theme={theme}
+                    key={header.id}
+                  >
+                    <div>
+                      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                      {{
+                        asc: (
+                          <Tooltip
+                            placement={TooltipPlacement.Bottom}
+                            isDark
+                            tooltipId="asc"
+                            triggerComponent={(setTriggerElement) => (
+                              <div ref={setTriggerElement}>
+                                <Icon
+                                  className="sort-icon"
+                                  name={IconName.ArrowSmallUp}
+                                  size={IconSize.MediumSmall}
+                                  color={IconColor.Support80}
+                                />
+                              </div>
+                            )}
+                          >
+                            <Text textStyle={TextStyle.TinyBold} color={TextColor.White}>
+                              Sorted A→Z
+                            </Text>
+                          </Tooltip>
+                        ),
+                        desc: (
+                          <Tooltip
+                            placement={TooltipPlacement.Bottom}
+                            isDark
+                            tooltipId="desc"
+                            triggerComponent={(setTriggerElement) => (
+                              <div ref={setTriggerElement}>
+                                <Icon
+                                  className="sort-icon"
+                                  name={IconName.ArrowSmallDown}
+                                  size={IconSize.MediumSmall}
+                                  color={IconColor.Support80}
+                                />
+                              </div>
+                            )}
+                          >
+                            <Text textStyle={TextStyle.TinyBold} color={TextColor.White}>
+                              Sorted Z→A
+                            </Text>
+                          </Tooltip>
+                        )
+                      }[header.column.getIsSorted() as string] ?? (
+                        <Tooltip
+                          placement={TooltipPlacement.Bottom}
+                          isDark
+                          tooltipId="normal"
+                          triggerComponent={(setTriggerElement) => (
+                            <div ref={setTriggerElement}>
+                              <StyledSortIcon
+                                theme={theme}
+                                name={IconName.Sort}
+                                size={IconSize.MediumSmall}
+                                color={IconColor.Support40}
+                              />
+                            </div>
+                          )}
+                        >
+                          <Text textStyle={TextStyle.TinyBold} color={TextColor.White}>
+                            Sort A→Z
+                          </Text>
+                        </Tooltip>
+                      )}
+                    </div>
+                  </StyledHead>
+                ))}
+              </tr>
+            ))}
+          </StyledThead>
+
+          <Droppable droppableId="tbody">
+            {(provided) => (
+              <StyledTbody ref={provided.innerRef} theme={theme}>
+                {data.length <= 0 && (
+                  <tr>
+                    <td colSpan={table.getFlatHeaders().length}>
+                      <StyledNoRecordsWrapper theme={theme}>
+                        <Text textStyle={TextStyle.MediumSmallDefault} color={TextColor.DigitalBlack}>
+                          No records found
+                        </Text>
+                      </StyledNoRecordsWrapper>
+                    </td>
+                  </tr>
+                )}
+                {table.getRowModel().rows.map((row) => (
+                  <React.Fragment key={row.id}>
+                    {isDraggable && (
+                      <DraggableRow
+                        tableContext={table}
+                        elementRef={nodesRef}
+                        row={row}
+                        allowSelection={allowSelection}
+                        onRowClick={() => onRowClick && onRowClick(row.original)}
+                        size={tableSize}
+                        dataTestId={dataTestId}
+                      />
+                    )}
+                    {!isDraggable && (
+                      <StyledRow
+                        theme={theme}
+                        key={row.id}
+                        onClick={() => onRowClick && onRowClick(row.original)}
+                        isClickable={!!onRowClick}
+                        size={tableSize}
+                      >
+                        {allowSelection && (
+                          <StyledTd data-testid={`${dataTestId}-cell-checkbox`} theme={theme}>
+                            <Checkbox onValueChange={row.getToggleSelectedHandler()} checked={row.getIsSelected()} />
+                          </StyledTd>
+                        )}
+                        {row.getVisibleCells().map((cell) => (
+                          <StyledTd
+                            alignPosition={
+                              table.getAllColumns()?.find((col) => col.columnDef.id === cell.column.id)?.columnDef
+                                ?.meta as TableHeaderAlignment | undefined
+                            }
+                            data-testid={`${dataTestId}-cell-${cell.id}`}
+                            theme={theme}
+                            key={cell.id}
+                          >
+                            {cell.renderValue() as ReactNode}
+                          </StyledTd>
+                        ))}
+                      </StyledRow>
+                    )}
+                  </React.Fragment>
+                ))}
+
+                {provided.placeholder}
+              </StyledTbody>
+            )}
+          </Droppable>
+        </StyledTable>
+        {data?.length > 0 && (
+          <StyledFooterWrapper data-testid={`${dataTestId}-pagination`} theme={theme}>
+            <StyledTextWrapper>
+              {table.getSelectedRowModel().flatRows.length > 0 && (
+                <Text textStyle={TextStyle.SmallDefault} color={TextColor.Support100}>
+                  {`Selecting ${table.getSelectedRowModel().flatRows.length} of ${data.length}`}
+                </Text>
+              )}
+            </StyledTextWrapper>
+            <StyledPaginationWrapper theme={theme}>
+              <Text color={TextColor.Support60} textStyle={TextStyle.SmallDefault}>
+                {`${pageIndex * pageSize + 1}-
+                ${dataPage.length < pageSize ? data.length : (pageIndex + 1) * pageSize} of
+                ${data.length} `}
+              </Text>
+              <Pagination
+                type={PaginationType.Input}
+                count={
+                  totalPagesCount ||
+                  (data.length / table.getState().pagination.pageSize -
+                    Math.floor(data.length / table.getState().pagination.pageSize) !==
+                  0
+                    ? Math.trunc(data.length / table.getState().pagination.pageSize + 1)
+                    : Math.trunc(data.length / table.getState().pagination.pageSize))
+                }
+                currentPage={table.getState().pagination.pageIndex}
+                onChange={(page) => handlePageChange(page)}
+                zeroBasedIndex
+              />
+            </StyledPaginationWrapper>
+          </StyledFooterWrapper>
+        )}
+      </div>
+    </DragDropContext>
   );
 };
