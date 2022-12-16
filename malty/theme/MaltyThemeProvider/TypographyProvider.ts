b86963668d5f74d7d8f135b145bf6a8b39d4d993
Gonçalo Almeida <goncalo.almeida@carlsberggroup.com>
2022-12-16 10:32:01 +0000

    feat(DSM-439): Change font-family behavior

--- malty/theme/MaltyThemeProvider/TypographyProvider.ts
+++ malty/theme/MaltyThemeProvider/TypographyProvider.ts
@@ -2,20 +2,9 @@ import { createGlobalStyle } from 'styled-components';
 
 const TypographyProvider = createGlobalStyle`
   @font-face {
-      font-family: 'Montserrat';
-      src: url('https://cdn.carlsberggroup.com/fonts/montserrat/Montserrat-VariableFont_wght.ttf');
-      font-display: 'swap';
-  }
-
-  :root {
-    font-family: 'Montserrat', sans-serif;
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    vertical-align: baseline;
-    line-height: 1;
-    width: auto;
+    font-family: 'Montserrat';
+    src: url('https://cdn.carlsberggroup.com/fonts/montserrat/Montserrat-VariableFont_wght.ttf');
+    font-display: 'swap';
   }
 `;
 
