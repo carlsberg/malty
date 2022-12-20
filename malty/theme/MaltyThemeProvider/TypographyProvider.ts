a626f2f5ff030ef24f8d580affce9cbdf15c4a5b
Gonçalo Almeida <goncalo.almeida@carlsberggroup.com>
2022-12-20 15:04:47 +0000

    feat(DSM-439): Add woff2 import and font specs

--- malty/theme/MaltyThemeProvider/TypographyProvider.ts
+++ malty/theme/MaltyThemeProvider/TypographyProvider.ts
@@ -3,8 +3,13 @@ import { createGlobalStyle } from 'styled-components';
 const TypographyProvider = createGlobalStyle`
   @font-face {
     font-family: 'Montserrat';
-    src: url('https://cdn.carlsberggroup.com/fonts/montserrat/Montserrat-VariableFont_wght.ttf');
+    src: url('https://cdn.carlsberggroup.com/fonts/montserrat/Montserrat-VF.woff2') format('woff2') tech('variations'),
+         url('https://cdn.carlsberggroup.com/fonts/montserrat/Montserrat-VF.woff2') format('woff2-variations'),
+         url('https://cdn.carlsberggroup.com/fonts/montserrat/Montserrat-VariableFont_wght.ttf') format('truetype') tech('variations'),
+         url('https://cdn.carlsberggroup.com/fonts/montserrat/Montserrat-VariableFont_wght.ttf') format('truetype-variations');
     font-display: 'swap';
+    font-weight: 400 900;
+    font-stretch: 100%;
   }
 `;
 
