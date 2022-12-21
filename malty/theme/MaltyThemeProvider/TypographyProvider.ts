8a3c9153784c718d1c34f243304108773a43c28b
Gonçalo Almeida <goncalo.almeida@carlsberggroup.com>
2022-12-21 13:53:30 +0000

    feat(DSM-439): Fix font-face declatration (#476)

--- malty/theme/MaltyThemeProvider/TypographyProvider.ts
+++ malty/theme/MaltyThemeProvider/TypographyProvider.ts
@@ -3,10 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 const TypographyProvider = createGlobalStyle`
   @font-face {
     font-family: 'Montserrat';
-    src: url('https://cdn.carlsberggroup.com/fonts/montserrat/Montserrat-VF.woff2') format('woff2') tech('variations'),
-         url('https://cdn.carlsberggroup.com/fonts/montserrat/Montserrat-VF.woff2') format('woff2-variations'),
-         url('https://cdn.carlsberggroup.com/fonts/montserrat/Montserrat-VariableFont_wght.ttf') format('truetype') tech('variations'),
-         url('https://cdn.carlsberggroup.com/fonts/montserrat/Montserrat-VariableFont_wght.ttf') format('truetype-variations');
+    src: url('https://cdn.carlsberggroup.com/fonts/montserrat/Montserrat-VF.woff2') format('woff2'),
+         url('https://cdn.carlsberggroup.com/fonts/montserrat/Montserrat-VariableFont_wght.ttf') format('truetype');
     font-display: 'swap';
     font-weight: 400 900;
     font-stretch: 100%;
