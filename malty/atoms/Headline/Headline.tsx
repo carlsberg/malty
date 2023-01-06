5a1415505cc8598d2af7d7ffcb27aa7bbc4717be
Gonçalo Almeida <goncalo.almeida@carlsberggroup.com>
2023-01-06 13:57:04 +0000

    feat(DSM-495): Update Headline as prop (#491)
    * feat(DSM-495): Update Headline as prop
    
    * fixup! feat(DSM-495): Update Headline as prop
    
    * fixup! fixup! feat(DSM-495): Update Headline as prop

--- malty/atoms/Headline/Headline.tsx
+++ malty/atoms/Headline/Headline.tsx
@@ -8,39 +8,50 @@ export const Headline = ({
   headlineStyle = HeadlineStyle.Medium,
   align = HeadlineAlign.Left,
   color = HeadlineColor.DigitalBlack,
-  as = 'h1',
-  children
+  as,
+  children,
 }: HeadlineProps) => {
   const theme = useContext(ThemeContext) || defaultTheme;
 
-  let StyledTagHeadline = as || 'h1';
+  let StyledTagHeadline = 'h1';
+  const allowedTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'span'];
 
-  switch (headlineStyle) {
-    case 'hero':
-      StyledTagHeadline = 'h2';
-      break;
-    case 'huge':
-      StyledTagHeadline = 'h3';
-      break;
-    case 'big':
-      StyledTagHeadline = 'h4';
-      break;
-    case 'large':
-      StyledTagHeadline = 'h5';
-      break;
-    case 'medium-large':
-      StyledTagHeadline = 'h6';
-      break;
-    case 'medium':
-      StyledTagHeadline = 'p';
-      break;
-    default:
-      StyledTagHeadline = 'h1';
-      break;
+  if (as && allowedTags.includes(as)) {
+    StyledTagHeadline = as;
+  } else {
+    switch (headlineStyle) {
+      case 'hero':
+        StyledTagHeadline = 'h2';
+        break;
+      case 'huge':
+        StyledTagHeadline = 'h3';
+        break;
+      case 'big':
+        StyledTagHeadline = 'h4';
+        break;
+      case 'large':
+        StyledTagHeadline = 'h5';
+        break;
+      case 'medium-large':
+        StyledTagHeadline = 'h6';
+        break;
+      case 'medium':
+        StyledTagHeadline = 'p';
+        break;
+      default:
+        StyledTagHeadline = 'h1';
+        break;
+    }
   }
 
   return (
-    <StyledHeadline as={StyledTagHeadline} headlineStyle={headlineStyle} align={align} color={color} theme={theme}>
+    <StyledHeadline
+      as={StyledTagHeadline}
+      headlineStyle={headlineStyle}
+      align={align}
+      color={color}
+      theme={theme}
+    >
       {children}
     </StyledHeadline>
   );
