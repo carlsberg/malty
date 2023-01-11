d7b33251b459923d1adc314bab051ce560baa68a
Gonçalo Almeida <goncalo.almeida@carlsberggroup.com>
2023-01-11 18:47:45 +0000

    feat(DSM-495): Fixed build warnings/errors

--- malty/atoms/Headline/Headline.tsx
+++ malty/atoms/Headline/Headline.tsx
@@ -2,46 +2,51 @@ import { globalTheme as defaultTheme } from '@carlsberggroup/malty.theme.malty-t
 import React, { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
 import { StyledHeadline } from './Headline.styled';
-import { HeadlineAlign, HeadlineColor, HeadlineProps, HeadlineStyle } from './Headline.types';
+import {
+  HeadlineAlign,
+  HeadlineColor,
+  HeadlineProps,
+  HeadlineStyle,
+  HeadlineTags,
+} from './Headline.types';
 
-export const Headline = ({
+export function Headline({
   headlineStyle = HeadlineStyle.Medium,
   align = HeadlineAlign.Left,
   color = HeadlineColor.DigitalBlack,
   as,
   children,
-}: HeadlineProps) => {
+}: HeadlineProps) {
   const theme = useContext(ThemeContext) || defaultTheme;
 
   let StyledTagHeadline = 'h1';
-  const allowedTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'span'];
 
-  if (as && allowedTags.includes(as)) {
+  switch (headlineStyle) {
+    case 'hero':
+      StyledTagHeadline = 'h2';
+      break;
+    case 'huge':
+      StyledTagHeadline = 'h3';
+      break;
+    case 'big':
+      StyledTagHeadline = 'h4';
+      break;
+    case 'large':
+      StyledTagHeadline = 'h5';
+      break;
+    case 'medium-large':
+      StyledTagHeadline = 'h6';
+      break;
+    case 'medium':
+      StyledTagHeadline = 'p';
+      break;
+    default:
+      StyledTagHeadline = 'h1';
+      break;
+  }
+
+  if (as && Object.values(HeadlineTags).includes(as)) {
     StyledTagHeadline = as;
-  } else {
-    switch (headlineStyle) {
-      case 'hero':
-        StyledTagHeadline = 'h2';
-        break;
-      case 'huge':
-        StyledTagHeadline = 'h3';
-        break;
-      case 'big':
-        StyledTagHeadline = 'h4';
-        break;
-      case 'large':
-        StyledTagHeadline = 'h5';
-        break;
-      case 'medium-large':
-        StyledTagHeadline = 'h6';
-        break;
-      case 'medium':
-        StyledTagHeadline = 'p';
-        break;
-      default:
-        StyledTagHeadline = 'h1';
-        break;
-    }
   }
 
   return (
@@ -55,4 +60,4 @@ export const Headline = ({
       {children}
     </StyledHeadline>
   );
-};
+}
--- malty/atoms/Headline/Headline.types.ts
+++ malty/atoms/Headline/Headline.types.ts
@@ -1,9 +1,11 @@
+import React from 'react';
+
 export interface HeadlineProps extends React.HTMLAttributes<HTMLHeadingElement> {
   headlineStyle?: HeadlineStyle;
   align?: HeadlineAlign;
   color?: HeadlineColor;
-  as?: React.ElementType;
-  children: string | JSX.Element;
+  as?: HeadlineTags;
+  children: React.ElementType;
 }
 
 export enum HeadlineStyle {
@@ -13,15 +15,26 @@ export enum HeadlineStyle {
   Big = 'big',
   Large = 'large',
   MediumLarge = 'medium-large',
-  Medium = 'medium'
+  Medium = 'medium',
 }
 export enum HeadlineAlign {
   Left = 'left',
   Center = 'center',
-  Right = 'right'
+  Right = 'right',
 }
 export enum HeadlineColor {
   ThemePrimary = 'theme-primary',
   DigitalBlack = 'digital-black',
-  White = 'white'
+  White = 'white',
+}
+
+export enum HeadlineTags {
+  H1 = 'h1',
+  H2 = 'h2',
+  H3 = 'h3',
+  H4 = 'h4',
+  H5 = 'h5',
+  H6 = 'h6',
+  P = 'p',
+  Span = 'span',
 }
