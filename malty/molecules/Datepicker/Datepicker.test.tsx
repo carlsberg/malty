098378c23b8e5d42359d26a3896b7a7f888e8d43
Gustavo Monteiro <gustavosmonteiro@gmail.com>
2023-01-20 14:21:53 +0000

    feat(DSM-513): Add new behaviors in Datepicker (#512)
    * feat(DSM-513): Add new behaviors in Datepicker
    
    * fix: Make PR request review changes

--- malty/molecules/Datepicker/Datepicker.test.tsx
+++ malty/molecules/Datepicker/Datepicker.test.tsx
@@ -1,5 +1,6 @@
 import { render } from '@carlsberggroup/malty.utils.test';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { Datepicker } from './Datepicker';
 
@@ -26,5 +27,40 @@ describe('datepicker', () => {
     );
     expect(screen.getByLabelText(label)).toBeInTheDocument();
     expect(screen.getByDisplayValue('01/01/2022 - 01/03/2022')).toBeInTheDocument();
+  });
+
+  it('opens datepicker when clicked', () => {
+    render(<Datepicker label={label} onChange={mockFn} startDate={new Date(2022, 0, 1)} />);
+    expect(screen.queryByText('January')).not.toBeInTheDocument();
+    userEvent.click(screen.getByLabelText(label));
+    expect(screen.getByText('January')).toBeInTheDocument();
+  });
+
+  it('renders datepicker within a Portal and close it when selecting date', () => {
+    render(<Datepicker label={label} onChange={mockFn} startDate={new Date(2022, 0, 1)} withPortal />);
+    userEvent.click(screen.getByLabelText(label));
+    expect(screen.getByText('January')).toBeInTheDocument();
+    const dayToBeClicked = screen.getByText('10');
+    userEvent.click(dayToBeClicked);
+    expect(mockFn).toHaveBeenCalled();
+    expect(screen.queryByText('January')).not.toBeInTheDocument();
+  });
+
+  it('renders datepicker within a Portal and not close it when selecting date if shouldCloseOnSelect is false', () => {
+    render(
+      <Datepicker
+        label={label}
+        onChange={mockFn}
+        startDate={new Date(2022, 0, 1)}
+        withPortal
+        shouldCloseOnSelect={false}
+      />
+    );
+    userEvent.click(screen.getByLabelText(label));
+    expect(screen.getByText('January')).toBeInTheDocument();
+    const dayToBeClicked = screen.getByText('10');
+    userEvent.click(dayToBeClicked);
+    expect(mockFn).toHaveBeenCalled();
+    expect(screen.getByText('January')).toBeInTheDocument();
   });
 });
