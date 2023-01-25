35b7981c30abc9673ff5287ee8774add102e5e59
Gonçalo Almeida <goncalo.almeida@carlsberggroup.com>
2023-01-25 15:35:20 +0000

    fix(DSM-380): Add storybook variant param to Card (#519)
    * feat(DSM-380): Add storybook variant param to Card
    
    * fixup! feat(DSM-380): Add storybook variant param to Card

--- malty/atoms/Card/Card.stories.tsx
+++ malty/atoms/Card/Card.stories.tsx
@@ -14,6 +14,7 @@ export default {
     importObject: 'Card',
     importPath: '@carlsberggroup/malty.atoms.Card',
     backgrounds: { name: 'dark background', value: '#000', default: true },
+    variants: ['shadowed', 'landscape', 'selected', 'onclick']
   },
   argTypes: {
     style: {
@@ -21,36 +22,36 @@ export default {
       options: Object.values(CardStyle),
       table: {
         defaultValue: {
-          summary: 'CardStyle.Plain',
-        },
+          summary: 'CardStyle.Plain'
+        }
       },
       control: {
-        type: 'select',
-      },
+        type: 'select'
+      }
     },
     orientation: {
       description: 'Card orientation. Options are',
       options: Object.values(CardOrientation),
       table: {
         defaultValue: {
-          summary: 'CardOrientation.Portrait',
-        },
+          summary: 'CardOrientation.Portrait'
+        }
       },
       control: {
-        type: 'select',
-      },
+        type: 'select'
+      }
     },
     selected: {
       control: 'boolean',
-      description: 'Selects the card',
+      description: 'Selects the card'
     },
     onClick: {
       description: 'This is a function that will run on click. It is not a required property',
       table: {
-        category: 'Events',
-      },
-    },
-  },
+        category: 'Events'
+      }
+    }
+  }
 };
 
 // eslint-disable-next-line react/function-component-definition
@@ -60,23 +61,121 @@ const Template: Story<CardProps> = (args) => {
 
 export const Card = Template.bind({});
 
-Card.args = {
-  selected: false,
-  style: CardStyle.Plain,
-  orientation: CardOrientation.Portrait,
-  cardHero: (
-    <div style={{ display: 'flex', alignItems: 'stretch', flexDirection: 'column', width: '100%' }}>
-      <Image src="https://placehold.co/300x180" />
-    </div>
-  ),
-  cardBody: (
-    <div>
-      <Headline headlineStyle={HeadlineStyle.Large}>Title</Headline>
-      <Text textStyle={TextStyle.MediumSmallBold}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua.
-      </Text>
-    </div>
-  ),
-  onClick: undefined,
-};
+const params = new URLSearchParams(window.location.search);
+const variant = params.get('variant');
+
+switch (variant) {
+  case 'shadowed':
+    Card.args = {
+      selected: false,
+      style: CardStyle.Shadowed,
+      orientation: CardOrientation.Portrait,
+      cardHero: (
+        <div style={{ display: 'flex', alignItems: 'stretch', flexDirection: 'column', width: '100%' }}>
+          <Image src="https://placehold.co/300x180" />
+        </div>
+      ),
+      cardBody: (
+        <div>
+          <Headline headlineStyle={HeadlineStyle.Large}>Title</Headline>
+          <Text textStyle={TextStyle.MediumSmallBold}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+          </Text>
+        </div>
+      ),
+      onClick: undefined
+    };
+    break;
+
+  case 'landscape':
+    Card.args = {
+      selected: false,
+      style: CardStyle.Plain,
+      orientation: CardOrientation.Landscape,
+      cardHero: (
+        <div style={{ display: 'flex', alignItems: 'stretch', flexDirection: 'column', width: '100%' }}>
+          <Image src="https://placehold.co/300x180" />
+        </div>
+      ),
+      cardBody: (
+        <div>
+          <Headline headlineStyle={HeadlineStyle.Large}>Title</Headline>
+          <Text textStyle={TextStyle.MediumSmallBold}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+          </Text>
+        </div>
+      ),
+      onClick: undefined
+    };
+    break;
+
+  case 'selected':
+    Card.args = {
+      selected: true,
+      style: CardStyle.Plain,
+      orientation: CardOrientation.Portrait,
+      cardHero: (
+        <div style={{ display: 'flex', alignItems: 'stretch', flexDirection: 'column', width: '100%' }}>
+          <Image src="https://placehold.co/300x180" />
+        </div>
+      ),
+      cardBody: (
+        <div>
+          <Headline headlineStyle={HeadlineStyle.Large}>Title</Headline>
+          <Text textStyle={TextStyle.MediumSmallBold}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+          </Text>
+        </div>
+      ),
+      onClick: undefined
+    };
+    break;
+
+  case 'onclick':
+    Card.args = {
+      selected: false,
+      style: CardStyle.Plain,
+      orientation: CardOrientation.Portrait,
+      cardHero: (
+        <div style={{ display: 'flex', alignItems: 'stretch', flexDirection: 'column', width: '100%' }}>
+          <Image src="https://placehold.co/300x180" />
+        </div>
+      ),
+      cardBody: (
+        <div>
+          <Headline headlineStyle={HeadlineStyle.Large}>Title</Headline>
+          <Text textStyle={TextStyle.MediumSmallBold}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+          </Text>
+        </div>
+      ),
+      onClick: () => alert("You've clicked the card!")
+    };
+    break;
+  default:
+    Card.args = {
+      selected: false,
+      style: CardStyle.Plain,
+      orientation: CardOrientation.Portrait,
+      cardHero: (
+        <div style={{ display: 'flex', alignItems: 'stretch', flexDirection: 'column', width: '100%' }}>
+          <Image src="https://placehold.co/300x180" />
+        </div>
+      ),
+      cardBody: (
+        <div>
+          <Headline headlineStyle={HeadlineStyle.Large}>Title</Headline>
+          <Text textStyle={TextStyle.MediumSmallBold}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+          </Text>
+        </div>
+      ),
+      onClick: undefined
+    };
+    break;
+}
