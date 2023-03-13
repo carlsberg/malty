b74c42052311458e44c08c297d57e6c31b066ec1
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2023-03-13 16:08:10 +0100

    Update check-tags

--- scripts/check-tags.js
+++ scripts/check-tags.js
@@ -1,13 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { execSync } = require('child_process');
 
 function hasUnstagedComponents() {
   const output = execSync('bit status -j', { encoding: 'utf8' });
-  const status = JSON.parse(output);
-  return status.modifiedComponent.length > 0 || status.newComponents.length > 0;
+  const { modifiedComponent, newComponents } = JSON.parse(output);
+  const hasUnstagedModifiedComponents = modifiedComponent.length > 0;
+  const hasUnstagedNewComponents = newComponents.length > 0;
+  const unstagedComponents = hasUnstagedModifiedComponents || hasUnstagedNewComponents;
+
+  if (unstagedComponents) {
+    console.error('Error: There are unstaged components. Please tag them and add a proper message for each of them.');
+    if (hasUnstagedModifiedComponents) {
+      console.error(`Modified components:\n - ${modifiedComponent.join('\n - ')}`);
+    }
+    if (hasUnstagedNewComponents) {
+      console.error(`New components:\n - ${newComponents.join('\n - ')}`);
+    }
+    process.exit(1);
+  }
 }
 
-// This will stop the .yml process
-if (hasUnstagedComponents()) {
-  console.error('Error: There are unstaged components. Please tag them and add a proper message for each of them.');
-  process.exit(1);
-}
+hasUnstagedComponents();
