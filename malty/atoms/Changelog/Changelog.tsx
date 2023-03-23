77c91eabcd5c6f771c14d263f0b38cd8885288c9
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2023-03-23 13:57:00 +0100

    Updater changelog

--- malty/atoms/Changelog/Changelog.tsx
+++ malty/atoms/Changelog/Changelog.tsx
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export const Changelog = () => <h1>This is a test component to check changelog changes 31</h1>;
+export const Changelog = () => <h1>This is a test component to check changelog changes 34</h1>;
