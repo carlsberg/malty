0f328651451688bfed2a69af38b90f15fba4793b
nataliaprikhoda <114430856+nataliaprikhoda@users.noreply.github.com>
2023-04-04 16:11:55 +0100

    add `roundMethod` prop

--- malty/atoms/ProgressCircle/ProgressCircle.tsx
+++ malty/atoms/ProgressCircle/ProgressCircle.tsx
@@ -3,29 +3,44 @@ import { globalTheme as defaultTheme } from '@carlsberggroup/malty.theme.malty-t
 import React, { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
 import { StyledBackgroundCircle, StyledForegroundCircle, StyledWrapper } from './ProgressCircle.styled';
-import { ForegroundCircleColor, PercentagePosition, ProgressCircleProps } from './ProgressCircle.types';
+import { ForegroundCircleColor, PercentagePosition, ProgressCircleProps, RoundMethod } from './ProgressCircle.types';
 
 export const ProgressCircle = ({
   foregroundColor = ForegroundCircleColor.DigitalBlack,
   displayPercentage = true,
   percentage,
-  percentagePosition = PercentagePosition.Left
+  percentagePosition = PercentagePosition.Left,
+  roundMethod = RoundMethod.Up
 }: ProgressCircleProps) => {
   const theme = useContext(ThemeContext) || defaultTheme;
 
-  const truncatedPercentage = Math.trunc(percentage);
+  let roundPercentage;
+  switch (roundMethod) {
+    case RoundMethod.Down: {
+      roundPercentage = Math.floor(percentage);
+      break;
+    }
+    case RoundMethod.Up: {
+      roundPercentage = Math.round(percentage);
+      break;
+    }
+    default: {
+      const exhaustiveCheck: never = roundMethod;
+      return exhaustiveCheck;
+    }
+  }
 
   let displayValue: number;
-  let label = `${truncatedPercentage} %`;
-  if (Number.isNaN(truncatedPercentage)) {
+  let label = `${roundPercentage} %`;
+  if (Number.isNaN(roundPercentage)) {
     displayValue = 0;
     label = 'Error';
-  } else if (truncatedPercentage > 100) {
+  } else if (roundPercentage > 100) {
     displayValue = 100;
-  } else if (truncatedPercentage < 0) {
+  } else if (roundPercentage < 0) {
     displayValue = 0;
   } else {
-    displayValue = truncatedPercentage;
+    displayValue = roundPercentage;
   }
 
   return (
