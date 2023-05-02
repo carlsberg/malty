6fc9223e628a1151b58799f907cf2a2d62057a9c
nataliaprikhoda <114430856+nataliaprikhoda@users.noreply.github.com>
2023-05-02 12:07:41 +0100

    add enums to export

--- malty/atoms/ProgressCircle/index.ts
+++ malty/atoms/ProgressCircle/index.ts
@@ -1,2 +1,3 @@
 export { ProgressCircle } from './ProgressCircle';
+export { ForegroundCircleColor, PercentagePosition, RoundMethod } from './ProgressCircle.types';
 export type { ProgressCircleProps } from './ProgressCircle.types';
