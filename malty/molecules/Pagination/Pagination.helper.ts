f5890b236e2f9c2a043f91a756deceeed78c4542
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2023-05-12 12:31:53 +0200

    Fix siblingCount logic

--- malty/molecules/Pagination/Pagination.helper.ts
+++ malty/molecules/Pagination/Pagination.helper.ts
@@ -2,6 +2,8 @@ import { useMemo } from 'react';
 
 export const LEFT_DOTS = 'left-dots';
 export const RIGHT_DOTS = 'right-dots';
+const BASE_PAGES_COUNT = 3; // firstPage + lastPage + currentPage
+const SIBLING_COUNT_FACTOR = 2;
 
 const range = (start: number, end: number) => {
   const length = end - start + 1;
@@ -21,49 +23,34 @@ export const usePagination = ({ totalPageCount, siblingCount = 1, currentPage, i
       return null;
     }
 
-    // Pages count is determined as siblingCount + firstPage + lastPage + currentPage + 2*DOTS
-    const totalPageNumbers = siblingCount + 5;
+    const renderedPages = BASE_PAGES_COUNT + siblingCount * SIBLING_COUNT_FACTOR;
 
-    /*
-      If the number of pages is less than the page numbers we want to show in our
-      paginationComponent, we return the range [1..totalPageCount]
-    */
-    if (totalPageNumbers >= totalPageCount) {
+    // We need to consider here the 2 DOTS
+    if (renderedPages + 2 >= totalPageCount) {
       return range(1, totalPageCount);
     }
 
-    const leftSiblingIndex = Math.max(currentPage - siblingCount, 1);
-    const rightSiblingIndex = Math.min(currentPage + siblingCount, totalPageCount);
+    const shouldDisplayLeftDots = currentPage > BASE_PAGES_COUNT + siblingCount;
+    const shouldDisplayRightDots = currentPage <= totalPageCount - (BASE_PAGES_COUNT + siblingCount);
 
-    /*
-      We do not want to show dots if there is only one position left
-      after/before the left/right page count as that would lead to a change if our Pagination
-      component size which we do not want
-    */
-    const shouldShowLeftDots = leftSiblingIndex > 2;
-    const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2;
+    if (shouldDisplayRightDots && shouldDisplayLeftDots) {
+      const leftSiblingIndex = currentPage - siblingCount;
+      const rightSiblingIndex = currentPage + siblingCount;
+      const middleRange = range(leftSiblingIndex, rightSiblingIndex);
 
-    const firstPageIndex = 1;
-    const lastPageIndex = totalPageCount;
+      return [1, LEFT_DOTS, ...middleRange, RIGHT_DOTS, totalPageCount];
+    }
 
-    if (!shouldShowLeftDots && shouldShowRightDots) {
-      const leftItemCount = 3 + 2 * siblingCount;
-      const leftRange = range(1, leftItemCount);
+    if (shouldDisplayRightDots) {
+      const leftRange = range(1, renderedPages);
 
       return [...leftRange, RIGHT_DOTS, totalPageCount];
     }
 
-    if (shouldShowLeftDots && !shouldShowRightDots) {
-      const rightItemCount = 3 + 2 * siblingCount;
-      const rightRange = range(totalPageCount - rightItemCount + 1, totalPageCount);
+    if (shouldDisplayLeftDots) {
+      const rightRange = range(totalPageCount - renderedPages + 1, totalPageCount);
 
-      return [firstPageIndex, LEFT_DOTS, ...rightRange];
-    }
-
-    if (shouldShowLeftDots && shouldShowRightDots) {
-      const middleRange = range(leftSiblingIndex, rightSiblingIndex);
-
-      return [firstPageIndex, LEFT_DOTS, ...middleRange, RIGHT_DOTS, lastPageIndex];
+      return [1, LEFT_DOTS, ...rightRange];
     }
 
     return null;
