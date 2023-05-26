8db2f63e99a9cfa05f17d1432db2347409791f29
Sergio Vanacloig <58294069+SergioVanacloigCarlsberg@users.noreply.github.com>
2023-05-26 10:50:45 +0200

    fix(input): [DSM-636] Validations for min and max quantity input (#635)
    * fix(input): [DSM-636] Min and max on quantity type
    
    * Add tests
    
    * Revert yarn.lock and rename val to newValue in tests
    
    * Revert yarn.lock changes
    
    ---------
    
    Co-authored-by: Sergio Vanacloig <sergiovanacloig97@gmail.com>

--- malty/atoms/Input/Input.helper.ts
+++ malty/atoms/Input/Input.helper.ts
@@ -12,3 +12,16 @@ export const useInputSize = ({ size }: UseInputSizeProps) => {
 
   return theme.sizes.xl.value.replace('px', '');
 };
+
+export const ensureQuantityRange = (value: number, min?: number, max?: number) => {
+  if (min !== undefined && max !== undefined) {
+    return Math.max(Math.min(value, max), min);
+  }
+  if (min !== undefined) {
+    return Math.max(value, min);
+  }
+  if (max !== undefined) {
+    return Math.min(value, max);
+  }
+  return value;
+};
--- malty/atoms/Input/Input.test.tsx
+++ malty/atoms/Input/Input.test.tsx
@@ -1,6 +1,7 @@
 import { render } from '@carlsberggroup/malty.utils.test';
 import { fireEvent, screen } from '@testing-library/react';
-import React from 'react';
+import userEvent from '@testing-library/user-event';
+import React, { useState } from 'react';
 import { Input } from './Input';
 import { InputType } from './Input.types';
 
@@ -84,5 +85,127 @@ describe('input', () => {
     const clearButton = screen.getByTestId(`icon-ItemClose`);
     fireEvent.click(clearButton);
     expect(onClearButtonClick).toHaveBeenCalledTimes(1);
+  });
+
+  describe(`Input type: ${InputType.Quantity}`, () => {
+    it('disables minus and plus buttons when value is equal to min and max', () => {
+      const handleValueChange = jest.fn();
+
+      render(
+        <Input
+          dataTestId="input"
+          value="0"
+          type={InputType.Quantity}
+          onValueChange={handleValueChange}
+          min={0}
+          max={0}
+        />
+      );
+
+      const minusButton = screen.getByTestId('input-quantity-minus');
+      const plusButton = screen.getByTestId('input-quantity-minus');
+
+      expect(minusButton).toBeDisabled();
+      expect(plusButton).toBeDisabled();
+
+      userEvent.click(minusButton);
+      userEvent.click(plusButton);
+
+      expect(handleValueChange).not.toHaveBeenCalled();
+    });
+
+    it('disables minus button when value is lower than min', () => {
+      const handleValueChange = jest.fn();
+
+      render(
+        <Input dataTestId="input" value="2" type={InputType.Quantity} onValueChange={handleValueChange} min={6} />
+      );
+
+      const minusButton = screen.getByTestId('input-quantity-minus');
+      expect(minusButton).toBeDisabled();
+
+      userEvent.click(minusButton);
+      expect(handleValueChange).not.toHaveBeenCalled();
+    });
+
+    it('disables plus button when value is greater than max', () => {
+      const handleValueChange = jest.fn();
+
+      render(
+        <Input dataTestId="input" value="12" type={InputType.Quantity} onValueChange={handleValueChange} max={10} />
+      );
+
+      const plusButton = screen.getByTestId('input-quantity-plus');
+      expect(plusButton).toBeDisabled();
+
+      userEvent.click(plusButton);
+      expect(handleValueChange).not.toHaveBeenCalled();
+    });
+
+    it('modifies the value to the min value passed if value is lower than min', () => {
+      const minValue = 6;
+      const handleOnChange = jest.fn();
+
+      const InputComponent = () => {
+        const [value, setValue] = useState('8');
+        const handleValueChange = (newValue: string) => {
+          setValue(newValue);
+          handleOnChange(newValue);
+        };
+
+        return (
+          <Input
+            dataTestId="input"
+            value={value}
+            type={InputType.Quantity}
+            onValueChange={handleValueChange}
+            min={minValue}
+          />
+        );
+      };
+
+      render(<InputComponent />);
+
+      const input = screen.getByTestId('input');
+
+      userEvent.clear(input);
+      userEvent.type(input, '4');
+
+      expect(handleOnChange).toHaveBeenCalledTimes(2);
+      expect(input).toHaveValue(minValue);
+    });
+
+    it('modifies the value to the max value passed if value is higher than max', () => {
+      const maxValue = 12;
+      const handleOnChange = jest.fn();
+
+      const InputComponent = () => {
+        const [value, setValue] = useState('10');
+        const handleValueChange = (newValue: string) => {
+          setValue(newValue);
+          handleOnChange(newValue);
+        };
+
+        return (
+          <Input
+            dataTestId="input"
+            value={value}
+            type={InputType.Quantity}
+            onValueChange={handleValueChange}
+            max={maxValue}
+          />
+        );
+      };
+
+      render(<InputComponent />);
+
+      const input = screen.getByTestId('input');
+
+      userEvent.clear(input);
+      userEvent.type(input, '16');
+
+      expect(handleOnChange).toHaveBeenCalledTimes(3);
+      expect(input).toHaveValue(maxValue);
+    });
   });
 });
