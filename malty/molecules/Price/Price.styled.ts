51dffde86efc38eeeb9962c36ed75cd142eb4ea5
Gustavo Monteiro <gustavosmonteiro@gmail.com>
2023-05-26 10:53:48 +0100

    add styles

--- malty/molecules/Price/Price.styled.ts
+++ malty/molecules/Price/Price.styled.ts
@@ -2,7 +2,9 @@ import { Text } from '@carlsberggroup/malty.atoms.text';
 import styled from 'styled-components';
 
 export const StyledPriceContainer = styled.div`
+  display: flex;
   flex-wrap: wrap;
+  align-items: center;
 `;
 
 export const StyledPrice = styled(Text)<{
