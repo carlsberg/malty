8b64af66335a86123e915def66a73c762e2b2f84
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2023-06-06 16:13:55 +0200

    Fix tests by mocking window.matchMedia

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,1 +1,14 @@
 import '@testing-library/jest-dom';
+
+window.matchMedia = jest.fn().mockImplementation(() => {
+  return {
+    matches: false,
+    media: '',
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn()
+  };
+});
