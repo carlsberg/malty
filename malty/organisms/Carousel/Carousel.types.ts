e0fea5e8654aded0523203d13ded498a68760f5d
Vitor Martins <ptmatvit@carlsberggroup.com>
2023-06-19 15:54:02 +0100

    feat(DSM-621): add CarouselItemProps to index file - review
    Co-authored-by: Sergio Vanacloig <58294069+SergioVanacloigCarlsberg@users.noreply.github.com>
    Signed-off-by: Vitor Martins  <ptmatvit@carlsberggroup.com>

--- malty/organisms/Carousel/Carousel.types.ts
+++ malty/organisms/Carousel/Carousel.types.ts
@@ -1,14 +1,18 @@
 import { Options, SplideProps } from '@splidejs/react-splide';
 
-export interface CarouselProps extends Pick<SplideProps, 'onVisible'> {
-  gap: Options['gap'];
-  perPage: Options['perPage'];
-  containerPaddingLeftAndRight?: number | string;
-  negative?: boolean;
-  breakpoints?: Record<string | number, Pick<Options, 'perPage' | 'gap'>>;
-  carouselSlide: CarouselItemProps[];
+export type CarouselProps = {
   dataTestId?: string;
-}
+  negative?: boolean;
+  carouselSlide: CarouselItemProps[];
+  containerPaddingLeftAndRight?: number | string;
+  breakpoints?: Record<string | number, Pick<Options, 'perPage' | 'gap'>>;
+} & Pick<SplideProps, 'onVisible'> &
+  Pick<Options, 'gap' | 'perPage'>;
+``
+
+I think in this case we should go for the Pick<> logic so we don't need to rewrite the name of the props again in case are the same and no need to extend from the Pick<SplideProps>
+ 
+On the CarouselItemProps I see the benefit of having the SplideProps['children'] so then we can rename the prop
 
 export interface CarouselItemProps {
   id?: string | number;
