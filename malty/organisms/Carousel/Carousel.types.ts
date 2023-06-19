42c8824c79351592fd9f8ceba9cc0ebc9300584e
vitor-martinss <vitormartinsweb@gmail.com>
2023-06-19 15:57:03 +0100

    feat(DSM-621): remove comments from review

--- malty/organisms/Carousel/Carousel.types.ts
+++ malty/organisms/Carousel/Carousel.types.ts
@@ -8,11 +8,6 @@ export type CarouselProps = {
   breakpoints?: Record<string | number, Pick<Options, 'perPage' | 'gap'>>;
 } & Pick<SplideProps, 'onVisible'> &
   Pick<Options, 'gap' | 'perPage'>;
-``
-
-I think in this case we should go for the Pick<> logic so we don't need to rewrite the name of the props again in case are the same and no need to extend from the Pick<SplideProps>
- 
-On the CarouselItemProps I see the benefit of having the SplideProps['children'] so then we can rename the prop
 
 export interface CarouselItemProps {
   id?: string | number;
