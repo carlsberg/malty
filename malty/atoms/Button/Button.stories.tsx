8793d5754bc84afafed3756c2558bd4e8fb6c19e
Gustavo Monteiro <gustavosmonteiro@gmail.com>
2023-06-20 10:06:15 +0100

    feat: Update storybook (WIP)

--- malty/atoms/Button/Button.stories.tsx
+++ malty/atoms/Button/Button.stories.tsx
@@ -1,219 +1,29 @@
-import { IconName } from '@carlsberggroup/malty.atoms.icon';
-import { Story } from '@storybook/react';
-import React from 'react';
-import { ButtonProps, ButtonSize } from '.';
-import { Button as ButtonComponent } from './Button';
-import { ButtonColor, ButtonIconPosition, ButtonStyle, ButtonType } from './Button.types';
+import type { Meta, StoryObj } from '@storybook/react';
 
-export default {
-  title: 'Forms/Button',
-  component: ButtonComponent,
-  parameters: {
-    importObject: 'Button',
-    importPath: '@carlsberggroup/malty.atoms.button',
-    variants: ['primary', 'secondary', 'transparent']
-  },
-  argTypes: {
-    text: {
-      control: 'text',
-      description: 'Button label, can be'
-    },
-    color: {
-      description: 'Button color. Options are',
-      options: Object.values(ButtonColor),
-      table: {
-        category: 'Styling',
-        defaultValue: {
-          summary: 'ButtonColor.DigitalBlack'
-        }
-      },
-      control: {
-        type: 'select'
-      }
-    },
-    type: {
-      description: 'The default behavior of the button. Possible values are',
-      options: Object.keys(ButtonType),
-      mapping: ButtonType,
-      control: {
-        type: 'select',
-        label: Object.values(ButtonType)
-      },
-      table: {
-        defaultValue: {
-          summary: 'ButtonType.Default'
-        }
-      }
-    },
-    style: {
-      description: 'CSS styling for the button, can be',
-      options: Object.keys(ButtonStyle),
-      mapping: ButtonStyle,
-      control: {
-        type: 'select',
-        label: Object.values(ButtonStyle)
-      },
-      table: {
-        category: 'Styling',
-        defaultValue: {
-          summary: 'ButtonStyle.Primary'
-        }
-      }
-    },
-    onClick: {
-      description: 'This is a function that will run on click. It is not a required property',
-      table: {
-        category: 'Events'
-      }
-    },
-    onKeyUp: {
-      description: 'This is a function that will run on onKeyUp. It is not a required property',
-      table: {
-        category: 'Events'
-      }
-    },
-    loading: {
-      table: {
-        category: 'State'
-      },
-      control: 'boolean',
-      description: 'Is button loading?'
-    },
-    size: {
-      description: 'Button size. Options are',
-      options: Object.values(ButtonSize),
-      table: {
-        category: 'Styling',
-        defaultValue: {
-          summary: 'ButtonSize.Medium'
-        }
-      },
-      control: {
-        type: 'select'
-      }
-    },
+import { Button } from './Button';
+import { ButtonStyle } from './Button.types';
 
-    icon: {
-      description: 'When selected, button label will contain the selected icon',
-      options: Object.values({ undefined, ...IconName }),
-      table: {
-        category: 'Icon'
-      },
-      control: {
-        type: 'select'
-      }
-    },
-    iconPos: {
-      description: 'When icon present, position will be',
-      options: Object.values(ButtonIconPosition),
-      table: {
-        category: 'Icon',
-        defaultValue: {
-          summary: 'Right'
-        }
-      },
-      control: {
-        type: 'select'
-      }
-    },
-    negative: {
-      control: 'boolean',
-      description: 'Should this be a white button?',
-      table: {
-        category: 'Styling'
-      }
-    },
-    disabled: {
-      table: {
-        category: 'State',
-        defaultValue: {
-          summary: 'false'
-        }
-      },
-      control: 'boolean',
-      description: 'Disable button'
-    },
-    fullWidth: {
-      control: 'boolean',
-      description: 'Should this be a full width button, that will stretch to 100% of its wrapper?',
-      table: {
-        category: 'Styling'
-      }
-    },
-    url: {
-      control: 'text',
-      description: 'Use this component as link or button with a simple URL, no click function needed'
-    },
-    selected: {
-      description: 'Add classname of "active" to element',
-      control: 'boolean',
-      table: {
-        category: 'Styling'
-      }
-    },
-    children: {
-      control: 'text',
-      description: 'Label your button with a child, as a string. This is an alternative to the `text` property'
-    }
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: 'Example/Button',
+  component: Button,
+  tags: ['autodocs']
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    style: ButtonStyle.Primary,
+    text: 'Button'
   }
 };
 
-const Template: Story<ButtonProps> = (args) => <ButtonComponent {...args} />;
-
-export const Button = Template.bind({});
-
-const params = new URLSearchParams(window.location.search);
-const variant = params.get('variant');
-
-switch (variant) {
-  case 'secondary':
-    Button.args = {
-      color: ButtonColor.DigitalBlack,
-      style: ButtonStyle.Secondary,
-      text: 'Secondary',
-      type: ButtonType.Submit,
-      size: ButtonSize.Medium,
-      iconPos: ButtonIconPosition.Right,
-      loading: false,
-      negative: false,
-      disabled: false,
-      fullWidth: false,
-      url: '',
-      selected: false
-    };
-    break;
-
-  case 'transparent':
-    Button.args = {
-      color: ButtonColor.DigitalBlack,
-      style: ButtonStyle.Transparent,
-      text: 'Transparent',
-      type: ButtonType.Submit,
-      size: ButtonSize.Medium,
-      iconPos: ButtonIconPosition.Right,
-      loading: false,
-      negative: false,
-      disabled: false,
-      fullWidth: false,
-      url: '',
-      selected: false
-    };
-    break;
-
-  default:
-    Button.args = {
-      color: ButtonColor.DigitalBlack,
-      style: ButtonStyle.Primary,
-      text: 'Primary',
-      type: ButtonType.Submit,
-      size: ButtonSize.Medium,
-      iconPos: ButtonIconPosition.Right,
-      loading: false,
-      negative: false,
-      disabled: false,
-      fullWidth: false,
-      url: '',
-      selected: false
-    };
-    break;
-}
+export const Secondary: Story = {
+  args: {
+    style: ButtonStyle.Secondary,
+    text: 'Button'
+  }
+};
