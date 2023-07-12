d684439c0a97dbc6290ec41c3a4c876e59658e8a
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2023-07-12 12:01:01 +0200

    Revert eslint changes

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,5 @@ export {};
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
-  moduleNameMapper: {
-    '@splidejs/react-splide/css/core$': 'identity-obj-proxy'
-  }
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts']
 };
