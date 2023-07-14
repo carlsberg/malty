b199ff5f316012ae14dcf42e9a29b65c0c1202cc
José Cavaleiro <zepedrocavaleiro@gmail.com>
2023-07-14 13:41:52 +0100

    updated indenting

--- malty/atoms/Chip/Chip.test.tsx
+++ malty/atoms/Chip/Chip.test.tsx
@@ -1,11 +1,11 @@
+import { IconName } from '@carlsberggroup/malty.atoms.icon';
+import { globalTheme as defaultTheme } from '@carlsberggroup/malty.theme.malty-theme-provider';
 import { render } from '@carlsberggroup/malty.utils.test';
-import { fireEvent, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { Chip } from './Chip';
 import { ChipSize } from './Chip.types';
-import { IconName, IconColor, IconSize, IconProps } from '@carlsberggroup/malty.atoms.icon';
-import { globalTheme as defaultTheme } from '@carlsberggroup/malty.theme.malty-theme-provider';
-import userEvent from '@testing-library/user-event';
 
 const defaultLabel = 'label';
 
@@ -18,7 +18,7 @@ describe('chip', () => {
 
   it('calls function onChange', () => {
     const onChange = jest.fn();
-    
+
     render(<Chip selected={false} label={defaultLabel} onChange={onChange} />);
 
     userEvent.click(screen.getByText(defaultLabel));
@@ -28,21 +28,21 @@ describe('chip', () => {
 
   it('calls function onClick', () => {
     const onClick = jest.fn();
-    render(<Chip selected={false} label={defaultLabel} onChange={onClick} />)
+    render(<Chip selected={false} label={defaultLabel} onChange={onClick} />);
 
     userEvent.click(screen.getByText(defaultLabel));
-    
+
     expect(onClick).toHaveBeenCalledTimes(1);
   });
 
   it('renders with the given test id', () => {
-    render(<Chip selected={false} label={defaultLabel} dataTestId='chip'/>);
+    render(<Chip selected={false} label={defaultLabel} dataTestId="chip" />);
 
     expect(screen.getByTestId('chip')).toBeVisible();
-  })
+  });
 
   it('displays add button', () => {
-    render(<Chip selected={false} label={defaultLabel} showAction/>);
+    render(<Chip selected={false} label={defaultLabel} showAction />);
 
     expect(screen.getByText(defaultLabel)).toBeInTheDocument();
     expect(screen.getByRole('button')).toBeInTheDocument();
@@ -50,7 +50,7 @@ describe('chip', () => {
   });
 
   it('checks if size is XSmall', () => {
-    render(<Chip selected={false} label={defaultLabel} size={ChipSize.XSmall} dataTestId='chip'/>);
+    render(<Chip selected={false} label={defaultLabel} size={ChipSize.XSmall} dataTestId="chip" />);
 
     expect(screen.getByTestId('chip')).toHaveStyle(
       `
@@ -60,7 +60,7 @@ describe('chip', () => {
   });
 
   it('checks if size is Small', () => {
-    render(<Chip selected={false} label={defaultLabel} size={ChipSize.Small} dataTestId='chip'/>);
+    render(<Chip selected={false} label={defaultLabel} size={ChipSize.Small} dataTestId="chip" />);
 
     expect(screen.getByTestId('chip')).toHaveStyle(
       `
@@ -70,7 +70,7 @@ describe('chip', () => {
   });
 
   it('checks if size is Medium', () => {
-    render(<Chip selected={false} label={defaultLabel} size={ChipSize.Medium} dataTestId='chip'/>);
+    render(<Chip selected={false} label={defaultLabel} size={ChipSize.Medium} dataTestId="chip" />);
 
     expect(screen.getByTestId('chip')).toHaveStyle(
       `
@@ -80,30 +80,30 @@ describe('chip', () => {
   });
 
   it('checks if its displaying the Alert Icon', () => {
-    render(<Chip selected={false} label={defaultLabel} icon={IconName.Alert} dataTestId='chip'/>);
+    render(<Chip selected={false} label={defaultLabel} icon={IconName.Alert} dataTestId="chip" />);
 
     expect(screen.getByTestId('icon-Alert')).toBeVisible();
   });
 
   it('checks if chip is disabled', () => {
     const onClick = jest.fn();
-    render(<Chip selected={false} label={defaultLabel} disabled dataTestId='chip'/>);
+    render(<Chip selected={false} label={defaultLabel} disabled dataTestId="chip" />);
 
     // TODO: remove skipPointerEventsCheck flag when user-event is bumped
-    userEvent.click(screen.getByTestId('chip'),undefined,{skipPointerEventsCheck: true});
+    userEvent.click(screen.getByTestId('chip'), undefined, { skipPointerEventsCheck: true });
 
-    expect(screen.getByTestId('chip')).toHaveAttribute("disabled","");
-    expect(onClick).toHaveBeenCalledTimes(0); 
+    expect(screen.getByTestId('chip')).toHaveAttribute('disabled', '');
+    expect(onClick).toHaveBeenCalledTimes(0);
   });
 
   it('checks if chip is readOnly', () => {
     const onClick = jest.fn();
-    render(<Chip selected={false} label={defaultLabel} readOnly dataTestId='chip'/>);
+    render(<Chip selected={false} label={defaultLabel} readOnly dataTestId="chip" />);
 
     // TODO: remove skipPointerEventsCheck flag when user-event is bumped
-    userEvent.click(screen.getByTestId('chip'),undefined,{skipPointerEventsCheck: true});
+    userEvent.click(screen.getByTestId('chip'), undefined, { skipPointerEventsCheck: true });
 
-    expect(screen.getByTestId('chip')).toHaveAttribute("readonly","");
+    expect(screen.getByTestId('chip')).toHaveAttribute('readonly', '');
     expect(onClick).toHaveBeenCalledTimes(0);
   });
 });
