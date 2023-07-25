dcb61f675024bb5ce52e6c709bd108b7fa66f75a
José Cavaleiro <zepedrocavaleiro@gmail.com>
2023-07-25 11:46:24 +0100

    feat(label): [DSM-717] Add tests

--- malty/atoms/Label/Label.test.tsx
+++ malty/atoms/Label/Label.test.tsx
@@ -6,8 +6,27 @@ import { Label } from '.';
 const defaultText = 'Test label';
 
 describe('label', () => {
-  it('renders with correct text', () => {
+  it('should render with correct text', () => {
     render(<Label label={defaultText} />);
+
     expect(screen.getByText(defaultText)).not.toBeNull();
+  });
+
+  it('should have disabled turned on', () => {
+    render(<Label dataTestId="Label" label={defaultText} />);
+
+    expect(screen.getByTestId('Label')).toHaveAttribute('disabled');
+  });
+
+  it('should have required turned on', () => {
+    render(<Label dataTestId="Label" label={defaultText} required />);
+
+    expect(screen.getByTestId('Label')).toHaveAttribute('required');
+  });
+
+  it('should have htmlFor with correct text', () => {
+    render(<Label dataTestId="Label" label={defaultText} htmlFor="htmlString" />);
+
+    expect(screen.getByTestId('Label')).toHaveAttribute('for', 'htmlString');
   });
 });
