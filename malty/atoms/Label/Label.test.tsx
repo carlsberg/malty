bf37bd005db369a2e4bdd08ffd846b2685204da6
José Cavaleiro <43344587+jpcav21@users.noreply.github.com>
2023-07-27 09:53:14 +0100

    feat(label): [DSM-717] Add tests (#716)
    * feat(label): [DSM-717] Add tests
    
    * removed disable and required tests

--- malty/atoms/Label/Label.test.tsx
+++ malty/atoms/Label/Label.test.tsx
@@ -6,8 +6,15 @@ import { Label } from '.';
 const defaultText = 'Test label';
 
 describe('label', () => {
-  it('renders with correct text', () => {
+  it('should render with correct text', () => {
     render(<Label label={defaultText} />);
+
     expect(screen.getByText(defaultText)).not.toBeNull();
+  });
+
+  it('should have htmlFor with correct text', () => {
+    render(<Label dataTestId="Label" label={defaultText} htmlFor="htmlString" />);
+
+    expect(screen.getByTestId('Label')).toHaveAttribute('for', 'htmlString');
   });
 });
