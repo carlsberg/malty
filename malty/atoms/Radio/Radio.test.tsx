034f8f6fd200ad165f020abd160f7925e6ac6f3d
José Cavaleiro <zepedrocavaleiro@gmail.com>
2023-08-01 10:04:06 +0100

    feat(radio): [DSM-725] Add tests

--- malty/atoms/Radio/Radio.test.tsx
+++ malty/atoms/Radio/Radio.test.tsx
@@ -1,10 +1,11 @@
 import { render } from '@carlsberggroup/malty.utils.test';
-import { fireEvent, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { Radio } from './Radio';
 
 describe('radio', () => {
-  it('renders elements', () => {
+  it('should render elements', () => {
     const mockFn = jest.fn();
     render(
       <Radio name="radio" label="Label text" error="Error text" value="Test value" onValueChange={mockFn} selected />
@@ -14,11 +15,35 @@ describe('radio', () => {
     expect(screen.getByDisplayValue('Test value')).toBeInTheDocument();
   });
 
-  it('calls function on click', () => {
+  it('should call function on click', () => {
     const mockFn = jest.fn();
     render(<Radio label="Label text" error="Error text" value="Test value" onValueChange={mockFn} name="radio" />);
     const radio = screen.getByDisplayValue('Test value');
-    fireEvent.click(radio);
+    userEvent.click(radio);
     expect(mockFn).toHaveBeenCalledTimes(1);
+  });
+
+  it('should be disabled', () => {
+    const mockFn = jest.fn();
+
+    render(
+      <Radio label="Label text" error="Error text" value="Test value" onValueChange={mockFn} name="radio" disabled />
+    );
+    const radio = screen.getByDisplayValue('Test value');
+
+    userEvent.click(radio, undefined, { skipPointerEventsCheck: true });
+
+    expect(mockFn).toHaveBeenCalledTimes(0);
+    expect(radio).toBeDisabled();
+  });
+
+  it('should have the correct data test id', () => {
+    const mockFn = jest.fn();
+
+    render(
+      <Radio dataTestId="radio" label="Label text" value="Test value" onValueChange={mockFn} name="radio" disabled />
+    );
+
+    expect(screen.getByTestId('radio')).toBeInTheDocument();
   });
 });
