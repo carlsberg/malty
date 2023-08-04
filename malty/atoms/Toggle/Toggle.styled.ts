4541d78c814c31b46461223b63bdb8c36781d4e5
acpcor <56742700+acpcor@users.noreply.github.com>
2023-08-04 17:27:23 +0100

    fix(toggle): [DSM-728] update some styles according to design specifications (#723)
    * fix(toggle): [DSM-728] update some styles according to the design specifications
    
    * fix(toggle): [DSM-728] improve border styles
    
    * fix(toggle): [DSM-728] update pseudo-elements syntax

--- malty/atoms/Toggle/Toggle.styled.ts
+++ malty/atoms/Toggle/Toggle.styled.ts
@@ -14,8 +14,8 @@ export const StyledLabelWrapper = styled.div<{
   disabled?: boolean;
 }>`
   align-items: center;
-  display: inline-block;
-  display: flex;
+  display: inline-flex;
+  cursor: pointer;
   // using hardcoded values due to not having token value and design team thinking on implementation with icons
   height: 14px;
   position: relative;
@@ -30,6 +30,25 @@ export const StyledLabelWrapper = styled.div<{
     box-shadow: none;
     outline: none;
   }
+
+  ${({ theme }) =>
+    theme &&
+    css`
+      &:hover {
+        input:checked + ${StyledSwitch} {
+          background-color: ${theme.colors.colours.overlay['digital-black'][75].value};
+          border-color: transparent;
+        }
+
+        ${StyledSwitch} {
+          border-color: ${theme.colors.colours.overlay['digital-black'][75].value};
+
+          &::before {
+            border-color: ${theme.colors.colours.overlay['digital-black'][75].value};
+          }
+        }
+      }
+    `}
 `;
 export const StyledToggleSwitch = styled.div`
   position: relative;
@@ -47,6 +66,7 @@ export const StyledLabel = styled(Label)<{
   disabled?: boolean;
   required?: boolean;
 }>`
+  cursor: pointer;
   font-weight: normal;
   font-family: ${({ theme }) => theme.typography.desktop.text['medium-small_default']['font-family'].value};
   font-size: ${({ theme }) => theme.typography.desktop.text['medium-small_default']['font-size'].value};
@@ -54,58 +74,18 @@ export const StyledLabel = styled(Label)<{
   margin-left: ${({ theme }) => theme.sizes['2xs'].value};
   margin-bottom: 0;
 `;
-export const StyledInput = styled.input<{
-  disabled?: boolean;
-}>`
-  display: none;
-  &:checked + .switch::before {
-    transform: translateX(14px);
-    background-color: ${({ theme }) => theme.colors.colours.default.white.value};
-    border: none;
-    top: 1px;
-  }
-  &:checked + .switch {
-    background-color: ${({ theme }) => theme.colors.colours.default['digital-black'].value};
-
-    &:hover {
-      ${({ theme }) =>
-        theme &&
-        css`
-          background-color: ${theme.colors.colours.overlay['digital-black'][75].value};
-          border: ${theme.borders['border-2px--solid']['border-width'].value}
-            ${theme.borders['border-2px--solid']['border-style'].value} transparent;
-        `}
-    }
-  }
-  &:focus,
-  &:focus-visible {
-    box-shadow: none;
-    outline: none;
-  }
-  ${({ disabled, theme }) =>
-    disabled &&
-    css`
-      &:checked + .switch {
-        background-color: ${theme.colors.colours.system['disable-light-theme'].value};
-        pointer-events: none;
-        border: ${theme.borders['border-2px--solid']['border-width'].value}
-          ${theme.borders['border-2px--solid']['border-style'].value} transparent;
-      }
-    `}
-`;
 
 export const StyledSwitch = styled.span<{
   disabled?: boolean;
 }>`
   position: absolute;
-  cursor: pointer;
   background-color: ${({ theme }) => theme.colors.colours.default.white.value};
   border-radius: ${({ theme }) => theme.sizes.m.value};
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
-  transition: transform 0.3s, background 0.3s;
+  transition: transform 0.3s ease-out;
 
   ${({ theme }) =>
     theme &&
@@ -113,19 +93,9 @@ export const StyledSwitch = styled.span<{
       border: ${theme.borders['border-2px--solid']['border-width'].value}
         ${theme.borders['border-2px--solid']['border-style'].value}
         ${theme.colors.colours.default['digital-black'].value};
-      &:hover {
-        border: ${theme.borders['border-2px--solid']['border-width'].value}
-          ${theme.borders['border-2px--solid']['border-style'].value}
-          ${theme.colors.colours.overlay['digital-black'][75].value};
-        &:before {
-          border: ${theme.borders['border-2px--solid']['border-width'].value}
-            ${theme.borders['border-2px--solid']['border-style'].value}
-            ${theme.colors.colours.overlay['digital-black'][75].value};
-        }
-      }
     `}
 
-  &:before {
+  &::before {
     position: absolute;
     content: '';
     left: -2px;
@@ -135,7 +105,7 @@ export const StyledSwitch = styled.span<{
     background-color: ${({ theme }) => theme.colors.colours.default.white.value};
 
     border-radius: 50%;
-    transition: transform 0.3s, background 0.3s;
+    transition: transform 0.3s ease-out;
     ${({ theme }) =>
       theme &&
       css`
@@ -147,15 +117,40 @@ export const StyledSwitch = styled.span<{
   ${({ disabled, theme }) =>
     disabled &&
     css`
-      &:before {
-        border: ${theme.borders['border-2px--solid']['border-width'].value}
-          ${theme.borders['border-2px--solid']['border-style'].value}
-          ${theme.colors.colours.system['disable-light-theme'].value};
+      &::before {
+        border-color: ${theme.colors.colours.system['disable-light-theme'].value};
         pointer-events: none;
       }
       pointer-events: none;
-      border: ${theme.borders['border-2px--solid']['border-width'].value}
-        ${theme.borders['border-2px--solid']['border-style'].value}
-        ${theme.colors.colours.system['disable-light-theme'].value};
+      border-color: ${theme.colors.colours.system['disable-light-theme'].value};
+    `}
+`;
+
+export const StyledInput = styled.input<{
+  disabled?: boolean;
+}>`
+  display: none;
+  &:checked + ${StyledSwitch}::before {
+    transform: translateX(14px);
+    background-color: ${({ theme }) => theme.colors.colours.default.white.value};
+    border: none;
+    top: 1px;
+  }
+  &:checked + ${StyledSwitch} {
+    background-color: ${({ theme }) => theme.colors.colours.default['digital-black'].value};
+  }
+  &:focus,
+  &:focus-visible {
+    box-shadow: none;
+    outline: none;
+  }
+  ${({ disabled, theme }) =>
+    disabled &&
+    css`
+      &:checked + ${StyledSwitch} {
+        background-color: ${theme.colors.colours.system['disable-light-theme'].value};
+        pointer-events: none;
+        border-color: transparent;
+      }
     `}
 `;
