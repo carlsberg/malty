be4d0a211331fc00a8f6805191651814fb3cefc7
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2023-10-03 11:46:55 +0200

    Remove extra lines and use es modules in main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,17 @@
 import { dirname, join } from 'path';
 
-module.exports = {
-  stories: ['../malty/!(icons)/!(IconWrapper)/*.stories.@(mdx|js|jsx|ts|tsx)'],
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
 
+export default {
+  stories: ['../malty/!(icons)/!(IconWrapper)/*.stories.@(mdx|js|jsx|ts|tsx)'],
   addons: [
     {
       name: '@storybook/addon-links',
       name: '@storybook/addon-essentials'
     }
   ],
-
   typescript: {
     check: false,
     checkOptions: {},
@@ -19,9 +21,7 @@ module.exports = {
       propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true)
     }
   },
-
   staticDirs: [{ from: '../public/storybook', to: '/' }],
-
   previewHead: (head) => `
     ${head}
     <style>
@@ -33,17 +33,11 @@ module.exports = {
       }
     </style>
   `,
-
   framework: {
     name: getAbsolutePath('@storybook/react-webpack5'),
     options: {}
   },
-
   docs: {
     autodocs: true
   }
 };
-
-function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
