2f90a34684b849f5e2297f82faf894092f2c0c60
José Cavaleiro <zepedrocavaleiro@gmail.com>
2023-10-09 10:20:21 +0100

    feat(headline): [DSM-834] add tests

--- malty/atoms/Headline/Headline.test.tsx
+++ malty/atoms/Headline/Headline.test.tsx
@@ -4,10 +4,19 @@ import React from 'react';
 import { Headline } from './Headline';
 
 const text = 'this is a headline';
+const testId = 'headline';
+
 describe('Headline component', () => {
   it('should render with the correct text', () => {
-    render(<Headline>{text}</Headline>);
+    render(<Headline>{text} </Headline>);
     const rendered = screen.getByText(text);
     expect(rendered).toBeInTheDocument();
+  });
+
+  it('should have the assigned data test id', () => {
+    render(<Headline dataTestId={testId}>{text} </Headline>);
+    const rendered = screen.getByText(text);
+    expect(rendered).toBeInTheDocument();
+    expect(rendered).toHaveAttribute('data-testid', testId);
   });
 });
