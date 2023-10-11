23513410f1896f8ff667768a9c17c816e2e0b586
José Cavaleiro <zepedrocavaleiro@gmail.com>
2023-10-11 16:08:15 +0100

    space refactoring

--- malty/atoms/Headline/Headline.test.tsx
+++ malty/atoms/Headline/Headline.test.tsx
@@ -8,7 +8,7 @@ const testId = 'headline';
 
 describe('Headline component', () => {
   it('should render with the correct text', () => {
-    render(<Headline>{text} </Headline>);
+    render(<Headline>{text}</Headline>);
 
     const rendered = screen.getByText(text);
 
@@ -16,7 +16,7 @@ describe('Headline component', () => {
   });
 
   it('should have the assigned data test id', () => {
-    render(<Headline dataTestId={testId}>{text} </Headline>);
+    render(<Headline dataTestId={testId}>{text}</Headline>);
 
     const rendered = screen.getByTestId(testId);
 
