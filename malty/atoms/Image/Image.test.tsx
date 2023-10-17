a55e490f2779bb23c350502b24ef172c257de4e3
José Cavaleiro <zepedrocavaleiro@gmail.com>
2023-10-17 10:07:26 +0100

    Added and updated tests

--- malty/atoms/Image/Image.test.tsx
+++ malty/atoms/Image/Image.test.tsx
@@ -1,5 +1,6 @@
 import { render } from '@carlsberggroup/malty.utils.test';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { Image } from '.';
 
@@ -8,10 +9,10 @@ const altText = 'Image Component';
 const dataTestId = 'image';
 
 describe('Image component', () => {
-  it('should renders the component with the correct alt description', () => {
+  it('should render the component with the correct alt description', () => {
     render(<Image src={testImageSrc} cover={false} alt={altText} dataTestId={dataTestId} />);
 
-    expect(screen.getAllByAltText(altText)).toBeDefined();
+    expect(screen.getByAltText(altText)).toBeDefined();
   });
 
   it('should render with the assigned dataTestid', () => {
@@ -23,18 +24,47 @@ describe('Image component', () => {
   it('should render with the correct caption', () => {
     render(<Image src={testImageSrc} cover={false} alt={altText} dataTestId={dataTestId} figcaption="caption text" />);
 
-    expect(screen.getByTestId('image-figcaption')).toHaveTextContent('caption text');
+    expect(screen.getByText('caption text')).toBeInTheDocument();
+  });
+  it('should render with the correct caption testId', () => {
+    render(<Image src={testImageSrc} cover={false} alt={altText} dataTestId={dataTestId} figcaption="caption text" />);
+
+    expect(screen.getByTestId('image-figcaption')).toBeInTheDocument();
   });
 
-  it('should render with the assigned height', () => {
-    render(<Image src={testImageSrc} cover={false} alt={altText} dataTestId={dataTestId} height="50" />);
+  it('should render the icon correctly in case an error occurs', () => {
+    render(<Image src="wrongSrc" cover={false} alt={altText} dataTestId={dataTestId} fallbackSrc="" />);
 
-    expect(screen.getByTestId(dataTestId)).toHaveAttribute('height', '50');
+    fireEvent.error(screen.getByTestId(dataTestId));
+
+    expect(screen.getByTestId('icon-Image')).toBeInTheDocument();
   });
 
-  it('should render with the assigned width', () => {
-    render(<Image src={testImageSrc} cover={false} alt={altText} dataTestId={dataTestId} width="50" />);
+  it('should render correctly using the fallbackSrc', () => {
+    render(<Image src="not a link" cover={false} alt={altText} dataTestId={dataTestId} fallbackSrc={testImageSrc} />);
 
-    expect(screen.getByTestId(dataTestId)).toHaveAttribute('width', '50');
+    fireEvent.error(screen.getByTestId(dataTestId));
+
+    expect(screen.getByTestId(dataTestId)).toBeInTheDocument();
+    expect(screen.getByAltText(altText)).toBeDefined();
+  });
+
+  it('should trigger the onClick function when clicked', () => {
+    const mockFn = jest.fn();
+    render(<Image src={testImageSrc} cover={false} alt={altText} dataTestId={dataTestId} onClick={mockFn} />);
+
+    userEvent.click(screen.getByTestId(dataTestId));
+
+    expect(mockFn).toHaveBeenCalled();
+  });
+
+  it('should render with the correct children', () => {
+    render(
+      <Image src={testImageSrc} cover={false} alt={altText} dataTestId={dataTestId}>
+        This is a sample string
+      </Image>
+    );
+
+    expect(screen.getByText('This is a sample string')).toBeInTheDocument();
   });
 });
