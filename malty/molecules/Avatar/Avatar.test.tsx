58f28d1a59fe3241b3aaa5a41a20934c89fbace5
José Cavaleiro <zepedrocavaleiro@gmail.com>
2023-10-23 16:26:57 +0100

    feat(avatar): [DSM-837] add tests

--- malty/molecules/Avatar/Avatar.test.tsx
+++ malty/molecules/Avatar/Avatar.test.tsx
@@ -1,5 +1,6 @@
 import { render } from '@carlsberggroup/malty.utils.test';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { Avatar } from './Avatar';
 
@@ -7,23 +8,46 @@ const imageSrc =
   'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80';
 
 describe('Avatar component', () => {
-  it('renders with the username initials', () => {
+  it('should render with the username initials', () => {
     render(<Avatar userName="John Doe" />);
     const initialsSpan = screen.getByText('JD');
     expect(initialsSpan).toBeDefined();
   });
 
-  it('renders with a profile image', () => {
+  it('should render with a profile image', () => {
     render(<Avatar profileImg={imageSrc} dataQaId="avatar" />);
     const avatarContainer = screen.getByTestId('avatar');
 
     expect(avatarContainer).toHaveStyle(`background-image: url(${imageSrc})`);
   });
 
-  it('when profile picture is available, do not render username initials', () => {
+  it('should not render username initials when profile picture is available', () => {
     render(<Avatar userName="John Doe" profileImg={imageSrc} />);
 
     const elementWithInitials = screen.queryByText('JD');
     expect(elementWithInitials).not.toBeInTheDocument();
+  });
+
+  it('should render with the correct data test id', () => {
+    render(<Avatar userName="John Doe" dataQaId="avatar" />);
+
+    expect(screen.getByTestId('avatar')).toBeInTheDocument();
+  });
+
+  it('should show loading indicator', () => {
+    render(<Avatar userName="John Doe" loading />);
+
+    expect(screen.getByTestId('undefined-pending-icon')).toBeInTheDocument();
+  });
+
+  it('should click the profile image', () => {
+    const mockFn = jest.fn();
+    render(<Avatar userName="John Doe" profileImg={imageSrc} onClick={mockFn} dataQaId="avatar" />);
+
+    const avatarContainer = screen.getByTestId('avatar');
+
+    userEvent.click(avatarContainer);
+
+    expect(mockFn).toHaveBeenCalledTimes(1);
   });
 });
