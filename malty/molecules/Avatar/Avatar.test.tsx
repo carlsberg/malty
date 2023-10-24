2dc54c94d99c9d7db788e8859a420fd094af4ba5
José Cavaleiro <zepedrocavaleiro@gmail.com>
2023-10-24 15:54:47 +0100

    new tests added and loading data test id issue fixed

--- malty/molecules/Avatar/Avatar.test.tsx
+++ malty/molecules/Avatar/Avatar.test.tsx
@@ -6,17 +6,26 @@ import { Avatar } from './Avatar';
 
 const imageSrc =
   'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80';
+const dataTestId = 'avatar';
 
 describe('Avatar component', () => {
   it('should render with the username initials', () => {
     render(<Avatar userName="John Doe" />);
     const initialsSpan = screen.getByText('JD');
-    expect(initialsSpan).toBeDefined();
+
+    expect(initialsSpan).toBeInTheDocument();
+  });
+
+  it('should render with first and last name initials when username has more than two words', () => {
+    render(<Avatar userName="John Doe Yea" />);
+    const initialsSpan = screen.getByText('JY');
+
+    expect(initialsSpan).toBeInTheDocument();
   });
 
   it('should render with a profile image', () => {
-    render(<Avatar profileImg={imageSrc} dataQaId="avatar" />);
-    const avatarContainer = screen.getByTestId('avatar');
+    render(<Avatar profileImg={imageSrc} dataQaId={dataTestId} />);
+    const avatarContainer = screen.getByTestId(dataTestId);
 
     expect(avatarContainer).toHaveStyle(`background-image: url(${imageSrc})`);
   });
@@ -29,25 +38,31 @@ describe('Avatar component', () => {
   });
 
   it('should render with the correct data test id', () => {
-    render(<Avatar userName="John Doe" dataQaId="avatar" />);
+    render(<Avatar userName="John Doe" dataQaId={dataTestId} />);
 
-    expect(screen.getByTestId('avatar')).toBeInTheDocument();
+    expect(screen.getByTestId(dataTestId)).toBeInTheDocument();
   });
 
   it('should show loading indicator', () => {
-    render(<Avatar userName="John Doe" loading />);
+    render(<Avatar userName="John Doe" loading dataQaId={dataTestId} />);
 
-    expect(screen.getByTestId('undefined-pending-icon')).toBeInTheDocument();
+    expect(screen.getByTestId('avatar-loading')).toBeInTheDocument();
   });
 
   it('should click the profile image', () => {
     const mockFn = jest.fn();
-    render(<Avatar userName="John Doe" profileImg={imageSrc} onClick={mockFn} dataQaId="avatar" />);
+    render(<Avatar userName="John Doe" profileImg={imageSrc} onClick={mockFn} dataQaId={dataTestId} />);
 
-    const avatarContainer = screen.getByTestId('avatar');
+    const avatarContainer = screen.getByTestId(dataTestId);
 
     userEvent.click(avatarContainer);
 
     expect(mockFn).toHaveBeenCalledTimes(1);
+  });
+
+  it('should render a customer icon when no username or image is passed', () => {
+    render(<Avatar />);
+
+    expect(screen.getByTestId('icon-Customer')).toBeInTheDocument();
   });
 });
