28a79379bf0aeaa908a75f641a1d3aa09593fede
José Cavaleiro <43344587+jpcav21@users.noreply.github.com>
2023-10-31 10:07:46 +0000

    feat(modal): [DSM-913] add tests (#826)
    * feat(modal): [DSM-913] add tests
    
    * added tests and refactored titles
    
    * added a new assertion
    
    * updated test
    
    ---------
    
    Signed-off-by: José Cavaleiro <43344587+jpcav21@users.noreply.github.com>

--- malty/molecules/Modal/Modal.test.tsx
+++ malty/molecules/Modal/Modal.test.tsx
@@ -29,8 +29,9 @@ const buttons: ActionButtonProps[] = [
 ];
 
 describe('modal', () => {
-  it('OnClick function passed successfully', () => {
+  it('should have onClick function working successfully', () => {
     render(<Modal open onClose={() => false} title={title} content={text} actions={buttons} />);
+
     const primaryButton = screen.getByText('Confirm');
 
     fireEvent(
@@ -40,13 +41,20 @@ describe('modal', () => {
         cancelable: true
       })
     );
+
     expect(screen.getByText('Clicked primary')).toBeInTheDocument();
   });
 
-  it('Modal closed correctly', () => {
+  it('should have modal closed correctly', () => {
+    const onClose = jest.fn();
     const ModalTest = () => {
       const [open, setOpen] = useState(true);
-      return <Modal open={open} onClose={() => setOpen(false)} title={title} content={text} actions={buttons} />;
+      const handleOnChange = () => {
+        setOpen(false);
+        onClose();
+      };
+
+      return <Modal open={open} onClose={handleOnChange} title={title} content={text} actions={buttons} />;
     };
 
     render(<ModalTest />);
@@ -60,6 +68,55 @@ describe('modal', () => {
         cancelable: true
       })
     );
+
+    expect(onClose).toHaveBeenCalledTimes(1);
     expect(screen.queryByText('Headline')).not.toBeInTheDocument();
+  });
+
+  it('should not render modal with open property set to false', () => {
+    const ModalTest = () => {
+      return <Modal open={false} onClose={() => false} title={title} content={text} actions={buttons} />;
+    };
+
+    render(<ModalTest />);
+
+    expect(screen.queryByText('Clicked Primary')).not.toBeInTheDocument();
+    expect(screen.queryByText('Headline')).not.toBeInTheDocument();
+  });
+
+  it('should not display close icon when dismissible is set as false', () => {
+    render(<Modal open onClose={() => false} title={title} content={text} actions={buttons} dismissible={false} />);
+
+    const closeIcon = screen.queryByTestId('icon-component');
+
+    expect(closeIcon).not.toBeInTheDocument();
+  });
+
+  it('should render with the correct headline', () => {
+    render(<Modal open onClose={() => false} title={title} content={text} actions={buttons} dismissible={false} />);
+
+    const headline = screen.getByText(title);
+
+    expect(headline).toBeInTheDocument();
+  });
+
+  it('should render with the correct content', () => {
+    render(<Modal open onClose={() => false} title={title} content={text} actions={buttons} dismissible={false} />);
+
+    const content = screen.getByText(
+      'Paragraph block to support main headline(optional) And…it can have 2 lines, more than that is just boring…'
+    );
+
+    expect(content).toBeInTheDocument();
+  });
+
+  it('should render with a custom button', () => {
+    const CustomButton = <button type="button">Custom text button</button>;
+
+    render(
+      <Modal open onClose={() => false} title={title} content={text} actions={CustomButton} dismissible={false} />
+    );
+
+    expect(screen.getByText('Custom text button')).toBeVisible();
   });
 });
