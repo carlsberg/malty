a28c4ec9a258b09ebb10f6e9d4a8b075801d431b
José Cavaleiro <zepedrocavaleiro@gmail.com>
2023-11-02 11:29:36 +0000

    feat(banner): [DSM-916] add tests

--- malty/molecules/Banner/Banner.test.tsx
+++ malty/molecules/Banner/Banner.test.tsx
@@ -1,12 +1,13 @@
 import { ButtonStyle } from '@carlsberggroup/malty.atoms.button';
 import { render } from '@carlsberggroup/malty.utils.test';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import React from 'react';
 import { Banner } from './Banner';
 import { ActionButtonProps } from './Banner.types';
 
 const title = 'Title';
 const description = 'This is a test';
+const label = 'label text';
 const imageSrc = 'https://placehold.co/300x180';
 const actions: ActionButtonProps[] = [
   {
@@ -22,17 +23,46 @@ const actions: ActionButtonProps[] = [
 ];
 
 describe('Banner', () => {
-  it('renders with correct basic content', () => {
+  it('should render with correct basic content', () => {
     render(<Banner title={title} imageSrc={imageSrc} />);
+
     expect(screen.getByText(title)).not.toBeNull();
     expect(screen.getByTestId('banner-component')).not.toBeNull();
   });
 
-  it('renders with correct full content', () => {
-    render(<Banner title={title} description={description} imageSrc={imageSrc} actions={actions} />);
+  it('should render with correct full content', () => {
+    render(<Banner title={title} description={description} label={label} imageSrc={imageSrc} actions={actions} />);
+
     expect(screen.getByText(title)).not.toBeNull();
     expect(screen.getByText(description)).not.toBeNull();
+    expect(screen.getByText(label)).not.toBeNull();
     expect(screen.getAllByRole('button')).toHaveLength(2);
     expect(screen.getByTestId('banner-component')).not.toBeNull();
+  });
+
+  it('should be able to click the buttons when rendered', () => {
+    render(<Banner title={title} description={description} label={label} imageSrc={imageSrc} actions={actions} />);
+
+    const primaryButton = screen.getByText('Cancel');
+    const secondaryButton = screen.getByText('Confirm');
+
+    fireEvent(
+      primaryButton,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true
+      })
+    );
+
+    fireEvent(
+      secondaryButton,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true
+      })
+    );
+
+    expect(screen.getByText('Cancel')).toBeInTheDocument();
+    expect(screen.getByText('Confirm')).toBeInTheDocument();
   });
 });
