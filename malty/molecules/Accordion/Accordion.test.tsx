ef5fcba913f2a55c73ee027d3ed68293527f46c9
José Cavaleiro <zepedrocavaleiro@gmail.com>
2023-11-02 10:40:56 +0000

    test updates and redundant test removed

--- malty/molecules/Accordion/Accordion.test.tsx
+++ malty/molecules/Accordion/Accordion.test.tsx
@@ -21,25 +21,6 @@ describe('Accordion', () => {
     expect(screen.getByText('Accordion title 1')).toBeInTheDocument();
     expect(screen.getByText('Accordion title 2')).toBeInTheDocument();
   });
-  it('should open accordion on click', () => {
-    render(
-      <Accordion size={AccordionSize.Medium}>
-        <AccordionItem eventKey="1" title="Accordion title 1">
-          <div>Accordion content 1</div>
-        </AccordionItem>
-        <AccordionItem eventKey="2" title="Accordion title 2">
-          <div>Accordion content 2</div>
-        </AccordionItem>
-      </Accordion>
-    );
-    const accordionItem = screen.getByText('Accordion title 1');
-
-    fireEvent.click(accordionItem);
-
-    const text = screen.queryByText('Accordion content 1');
-
-    expect(text).toBeVisible();
-  });
 
   it('should have the correct test id', () => {
     render(
@@ -74,12 +55,14 @@ describe('Accordion', () => {
     fireEvent.click(accordionItem1);
     fireEvent.click(accordionItem2);
 
-    const text = screen.queryByText('Accordion content 1');
+    const text1 = screen.queryByText('Accordion content 1');
+    const text2 = screen.queryByText('Accordion content 2');
 
-    expect(text).toBeVisible();
+    expect(text1).toBeVisible();
+    expect(text2).toBeVisible();
   });
 
-  it('should behave properly when an new item is opened the other one closes', () => {
+  it('should open on click and have default behavior working properly so that when an new item is opened the other one closes', () => {
     render(
       <Accordion size={AccordionSize.Medium} dataQaId="accordion">
         <AccordionItem eventKey="1" title="Accordion title 1">
