1740db3fa000f40891910c0aabe92c70e58de8ca
José Cavaleiro <43344587+jpcav21@users.noreply.github.com>
2023-11-07 09:46:51 +0000

    feat(banner): [DSM-916] add tests (#834)
    * feat(banner): [DSM-916] add tests
    
    * added and updated tests

--- malty/molecules/Banner/Banner.test.tsx
+++ malty/molecules/Banner/Banner.test.tsx
@@ -1,38 +1,66 @@
 import { ButtonStyle } from '@carlsberggroup/malty.atoms.button';
 import { render } from '@carlsberggroup/malty.utils.test';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { Banner } from './Banner';
 import { ActionButtonProps } from './Banner.types';
 
 const title = 'Title';
 const description = 'This is a test';
+const label = 'label text';
 const imageSrc = 'https://placehold.co/300x180';
+const mockFn = jest.fn();
 const actions: ActionButtonProps[] = [
   {
     key: 'cancel',
     style: ButtonStyle.Primary,
-    text: 'Cancel'
+    text: 'Cancel',
+    onClick: mockFn
   },
   {
     key: 'confirm',
     style: ButtonStyle.Secondary,
-    text: 'Confirm'
+    text: 'Confirm',
+    onClick: mockFn
   }
 ];
 
 describe('Banner', () => {
-  it('renders with correct basic content', () => {
+  it('should render with correct basic content', () => {
     render(<Banner title={title} imageSrc={imageSrc} />);
-    expect(screen.getByText(title)).not.toBeNull();
-    expect(screen.getByTestId('banner-component')).not.toBeNull();
+
+    expect(screen.getByText(title)).toBeInTheDocument();
+    expect(screen.getByTestId('banner-component')).toBeInTheDocument();
   });
 
-  it('renders with correct full content', () => {
-    render(<Banner title={title} description={description} imageSrc={imageSrc} actions={actions} />);
-    expect(screen.getByText(title)).not.toBeNull();
-    expect(screen.getByText(description)).not.toBeNull();
+  it('should render with correct full content', () => {
+    render(<Banner title={title} description={description} label={label} imageSrc={imageSrc} actions={actions} />);
+
+    expect(screen.getByText(title)).toBeInTheDocument();
+    expect(screen.getByText(description)).toBeInTheDocument();
+    expect(screen.getByText(label)).toBeInTheDocument();
     expect(screen.getAllByRole('button')).toHaveLength(2);
-    expect(screen.getByTestId('banner-component')).not.toBeNull();
+    expect(screen.getByTestId('banner-component')).toBeInTheDocument();
+  });
+
+  it('should be able to click the buttons when rendered', () => {
+    render(<Banner title={title} description={description} label={label} imageSrc={imageSrc} actions={actions} />);
+
+    const primaryButton = screen.getByText('Cancel');
+    const secondaryButton = screen.getByText('Confirm');
+
+    userEvent.click(primaryButton);
+    userEvent.click(secondaryButton);
+
+    expect(mockFn).toHaveBeenCalledTimes(2);
+  });
+
+  it('should render with a custom button', () => {
+    const CustomButton = <button type="button">Custom text button</button>;
+
+    render(<Banner title={title} description={description} label={label} imageSrc={imageSrc} actions={CustomButton} />);
+
+    expect(screen.getByText('Custom text button')).toBeVisible();
   });
 });
