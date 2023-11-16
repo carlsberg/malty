4e79e6e079b2081e2e9c0b245fa7db2e5f79ae49
Samuel Silva <samuelsilvawb@gmail.com>
2023-11-16 09:18:48 +0000

    fix: export padded container size

--- malty/atoms/PaddedContainer/index.ts
+++ malty/atoms/PaddedContainer/index.ts
@@ -1,2 +1,3 @@
 export { PaddedContainer } from './PaddedContainer';
+export { PaddedContainerSize } from './PaddedContainer.types';
 export type { PaddedContainerProps } from './PaddedContainer.types';
