af7606fb7e6ba0d20de2c817cf16dbd9c00bbdb6
acpcor <PTCORANA@carlsberggroup.com>
2023-11-21 17:43:54 +0000

    remove progress-spinner component

--- malty/atoms/ProgressSpinner/index.ts
+++
@@ -1,3 +0,0 @@
-export { ProgressSpinner } from './ProgressSpinner';
-export { ProgressSpinnerColor, ProgressSpinnerSize, ProgressSpinnerStatus } from './ProgressSpinner.types';
-export type { ProgressSpinnerProps } from './ProgressSpinner.types';
