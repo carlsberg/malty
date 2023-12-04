142b81fd9465a4127bdac6dc2aa85f07946366a2
José Cavaleiro <zepedrocavaleiro@gmail.com>
2023-12-04 13:11:03 +0000

    feat(price): [DSM-921] add tests

--- malty/atoms/Price/Price.test.tsx
+++ malty/atoms/Price/Price.test.tsx
@@ -6,47 +6,60 @@ import { Price } from './Price';
 const base = '1000';
 const discount = '500';
 const credit = '200';
+const testId = 'price';
 
 describe('Price', () => {
-  test('renders default only correctly', () => {
+  it('should renders default only correctly', () => {
     render(<Price base={base} />);
+
     expect(screen.getByText(base)).toBeInTheDocument();
     expect(screen.queryByText(discount)).not.toBeInTheDocument();
     expect(screen.queryByText(credit)).not.toBeInTheDocument();
   });
 
-  test('renders discount only correctly', () => {
+  it('should render discount only correctly', () => {
     render(<Price discount={discount} />);
+
     expect(screen.queryByText(base)).not.toBeInTheDocument();
     expect(screen.getByText(discount)).toBeInTheDocument();
     expect(screen.queryByText(credit)).not.toBeInTheDocument();
   });
 
-  test('renders credit only correctly', () => {
+  it('should render credit only correctly', () => {
     render(<Price credit={credit} />);
+
     expect(screen.queryByText(base)).not.toBeInTheDocument();
     expect(screen.queryByText(discount)).not.toBeInTheDocument();
     expect(screen.getByText(credit)).toBeInTheDocument();
   });
 
-  test('renders default and discount correctly', () => {
+  it('should render default and discount correctly', () => {
     render(<Price base={base} discount={discount} />);
+
     expect(screen.getByText(base)).toBeInTheDocument();
     expect(screen.getByText(discount)).toBeInTheDocument();
     expect(screen.queryByText(credit)).not.toBeInTheDocument();
   });
 
-  test('renders default, discount and credit correctly', () => {
+  it('should render default, discount and credit correctly', () => {
     render(<Price base={base} discount={discount} credit={credit} />);
+
     expect(screen.queryByText(base)).not.toBeInTheDocument();
     expect(screen.queryByText(discount)).not.toBeInTheDocument();
     expect(screen.getByText(credit)).toBeInTheDocument();
   });
 
-  test('renders discount and credit correctly', () => {
+  it('should render discount and credit correctly', () => {
     render(<Price discount={discount} credit={credit} />);
+
     expect(screen.queryByText(base)).not.toBeInTheDocument();
     expect(screen.queryByText(discount)).not.toBeInTheDocument();
     expect(screen.getByText(credit)).toBeInTheDocument();
+  });
+
+  it('should render with the correct test id', () => {
+    render(<Price discount={discount} credit={credit} dataTestId={testId} />);
+
+    expect(screen.getByTestId(`${testId}-credit-price`)).toBeInTheDocument();
   });
 });
