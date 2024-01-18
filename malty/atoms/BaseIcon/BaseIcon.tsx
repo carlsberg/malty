989d426f96ff1637786de385c74dfee3373e8dac
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2024-01-18 12:45:46 +0100

    Add className on icon also

--- malty/atoms/BaseIcon/BaseIcon.tsx
+++ malty/atoms/BaseIcon/BaseIcon.tsx
@@ -24,6 +24,7 @@ export const BaseIcon = ({
       data-testid={dataTestId}
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
+      className={className}
       {...props}
     >
       {children}
