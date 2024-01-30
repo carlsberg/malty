01baa5e550a960e606c8c979159b04aa311d5040
Samuel Silva <samuelsilvawb@gmail.com>
2024-01-30 14:28:30 +0000

    test: cover base icon implementation

--- malty/atoms/BaseIcon/BaseIcon.test.tsx
+++ malty/atoms/BaseIcon/BaseIcon.test.tsx
@@ -1,20 +1,42 @@
+import { CarlsbergFilled } from '@carlsberggroup/malty.icons.carlsberg-filled';
 import { render } from '@carlsberggroup/malty.utils.test';
 import { screen } from '@testing-library/react';
 import React from 'react';
-import { BaseIcon } from './BaseIcon';
+import { BaseIcon, CloneIcon } from './BaseIcon';
+
+const dataTestId = 'base-icon-component';
 
 describe('BaseIcon', () => {
   it('should render default "dataTestId"', () => {
     render(<BaseIcon />);
 
     expect(screen.getByTestId('icon')).toBeVisible();
+    expect(screen.queryByRole('button')).not.toBeInTheDocument();
   });
 
   it('should render component with the new "dataTestId"', () => {
-    const dataTestId = 'base-icon-component';
-
     render(<BaseIcon dataTestId={dataTestId} />);
 
     expect(screen.getByTestId(dataTestId)).toBeVisible();
+  });
+
+  it('should render a button when an `onClick` is passed', () => {
+    const ariaLabel = 'BaseIcon being displayed';
+
+    render(<BaseIcon dataTestId={dataTestId} onClick={jest.fn()} ariaLabel={ariaLabel} />);
+
+    expect(screen.getByRole('button', { name: ariaLabel })).toBeVisible();
+  });
+
+  it('should clone an icon correctly', () => {
+    render(<CloneIcon icon={<CarlsbergFilled dataTestId={dataTestId} />} />);
+
+    expect(screen.getByTestId(dataTestId)).toBeVisible();
+  });
+
+  it('should not break if icon is not present', () => {
+    const { container } = render(<CloneIcon />);
+
+    expect(container).toBeEmptyDOMElement();
   });
 });
