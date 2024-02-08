87d0f77f0a278f8dcf6769d39b5813389d5ad579
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2024-02-08 18:14:39 +0100

    Add dataTestid on overlay

--- malty/atoms/Overlay/Overlay.tsx
+++ malty/atoms/Overlay/Overlay.tsx
@@ -4,11 +4,11 @@ import { ThemeContext } from 'styled-components';
 import { OverlayProps } from '.';
 import { StyledOverlay } from './Overlay.styled';
 
-export const Overlay = ({ content, isWhite = false, zIndex = 0, fixed = true }: OverlayProps) => {
+export const Overlay = ({ content, isWhite = false, zIndex = 0, fixed = true, dataTestId }: OverlayProps) => {
   const theme = useContext(ThemeContext) || defaultTheme;
 
   return (
-    <StyledOverlay zIndex={zIndex} isWhite={isWhite} theme={theme} $fixed={fixed}>
+    <StyledOverlay zIndex={zIndex} isWhite={isWhite} theme={theme} $fixed={fixed} data-testid={dataTestId}>
       {content}
     </StyledOverlay>
   );
