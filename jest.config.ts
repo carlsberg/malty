edf8e7aac20b1111679632f9670692369823e188
Tiago Teixeira <ptteitia@carlsberggroup.com>
2024-06-03 17:05:06 +0100

    feat: configure scripts for unit tests and remove changelog files

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,14 @@
-export {};
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts']
+  moduleDirectories: ['node_modules', './malty'],
+  moduleNameMapper: {
+    '^@atoms/(.*)$': './malty/atoms/$1',
+    '^@molecules/(.*)$': './malty/molecules/$1',
+    '^@organisms/(.*)$': './malty/organisms/$1',
+    '^@utils/(.*)$': './malty/utils/$1',
+    '^@theme/(.*)$': './malty/theme/$1',
+    '^@icons/(.*)$': './malty/icons/$1'
+  },
+  setupFilesAfterEnv: ['./jest.setup.ts']
 };
