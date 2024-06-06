c9ede8fcabc2f4f0e311f1785c3cf6d5f2cdb9b2
Tiago Teixeira <ptteitia@carlsberggroup.com>
2024-06-06 15:06:36 +0100

    feat: add new theme provider to lerna

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
-import { MaltyThemeProvider } from '@carlsberggroup/malty.theme.malty-theme-provider';
-import { defaultTheme, NewMaltyThemeProvider } from '@carlsberggroup/malty.theme.new-malty-theme-provider';
+import { MaltyThemeProvider } from '@carlsberggbs/malty.theme.malty-theme-provider';
+import { defaultTheme, NewMaltyThemeProvider } from '@carlsberggbs/malty.theme.new-malty-theme-provider';
 import { Controls, Description, DocsContainer, Primary, Stories, Subtitle, Title } from '@storybook/addon-docs';
 import React from 'react';
 import styled from 'styled-components';
