1285a6c9df0259b489292c722e07fba7edbfb411
Tiago Teixeira <ptteitia@carlsberggroup.com>
2024-06-20 17:38:27 +0100

    fix: test bumping one component only - Button (#1013)
    # What
    
    <!-- Describe the technical "why" behind your changes -->
    
    ## Code Quality Checklist
    
    - [ ] Lint and unit tests pass locally with my changes
    - [ ] I have added tests that prove my fix is effective or that my
    feature works (if appropriate)
    - [ ] I have added necessary documentation (if appropriate)
    - [ ] I have updated storybook (if appropriate)
    - [ ] I have ran `yarn set-peer-dep` when adding an icon as a dependency
    of another component (if appropriate) [read
    more](https://carlsberggbs.atlassian.net/wiki/spaces/DSM/pages/4697162121/Components+as+peer+dependency)
    
    ## Jira Card
    
    <!-- if no card is associated, remove the line bellow and add "Not
    Applicable" -->
    
    DSM-

--- malty/atoms/Button/Button.tsx
+++ malty/atoms/Button/Button.tsx
@@ -127,7 +127,7 @@ export const Button = ({
       </div>
       {loading && (
         <div data-testid={`${dataTestId}-loading`} className="secondary-container">
-          <Loading color={getLoadingColor()} negative={getIsLoadingNegative()} status={LoadingStatus.Pending} />
+          <Loading color={getLoadingColor()} status={LoadingStatus.Pending} negative={getIsLoadingNegative()} />
         </div>
       )}
     </Component>
