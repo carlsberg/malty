4f6ab6ff0a824bee95dcabfce08df5bd4452c26a
Tiago Teixeira <ptteitia@carlsberggroup.com>
2024-06-20 16:57:17 +0100

    fix: test versioning workflow when modify one component: Button

--- malty/atoms/Button/Button.tsx
+++ malty/atoms/Button/Button.tsx
@@ -127,7 +127,7 @@ export const Button = ({
       </div>
       {loading && (
         <div data-testid={`${dataTestId}-loading`} className="secondary-container">
-          <Loading color={getLoadingColor()} status={LoadingStatus.Pending} negative={getIsLoadingNegative()} />
+          <Loading status={LoadingStatus.Pending} negative={getIsLoadingNegative()} color={getLoadingColor()} />
         </div>
       )}
     </Component>
